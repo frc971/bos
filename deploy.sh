08745800cc05f5c4cb18b87c0be951d1eb6380fe
Your Name <you@example.com>
2025-08-30 06:15:58 +0000

    build
    Signed-off-by: Your Name <you@example.com>

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+executables=("main/main" "main/calibration/frame_logger")
+
+for n in $executables; do
+  cp $n bin
+ done
+
+
+mkdir -p bin
+for n in `find -type d -name "build" -prune -name "*.so"`; do
+  cp $n bin
+ done
