34b9e30f9d0f61a2b8a12225eb4309ee7b867854
Your Name <you@example.com>
2025-09-05 20:56:10 -0700

    frame logger working
    Signed-off-by: Your Name <you@example.com>
    
    Former-commit-id: f0d3d5cfb3d01a93ae38311655e2afbdd5783c94

--- main/camera/streamer.cc
+++ main/camera/streamer.cc
@@ -10,18 +10,21 @@ static std::string k_header =
     "Pragma: no-cache\r\n"
     "Content-Type: multipart/x-mixed-replace; boundary=frame\r\n\r\n";
 
-Streamer::Streamer(uint port, bool verbose) : status_(false){
+Streamer::Streamer(uint port, bool verbose, uint skip_frame) : status_(false), verbose_(verbose), skip_frame_(skip_frame), skip_frame_idx_(0){
   address_length_ = sizeof(address_);
 
   server_fd_ = socket(AF_INET, SOCK_STREAM, 0);
-  if (server_fd_ == -1) { perror("socket"); return; }
+  if (server_fd_ == -1) { 
+    std::cout << "failed to open socket!\n";
+    return;
+  }
 
   address_.sin_family = AF_INET;
   address_.sin_addr.s_addr = INADDR_ANY;
   address_.sin_port = htons(port);
 
   if (bind(server_fd_, (struct sockaddr*)&address_, sizeof(address_)) < 0) {
-    perror("bind"); 
+    std::cout << "failed to bind!";
     return;
   }
 
@@ -29,38 +32,48 @@ Streamer::Streamer(uint port, bool verbose) : status_(false){
   if (verbose){
     std::cout << "HTTP MJPEG server running on port " << port << std::endl;
   }
-  listen_thread_  = std::thread(&Streamer::Listen, this);
+  // listen_thread_  = std::thread(&Streamer::Listen, this);
+  std::cout << "waiting for client..." << std::endl;
+  client_fd_ = accept(server_fd_, (struct sockaddr*)&address_, &address_length_);
+  if (client_fd_ < 0){
+    std::cout << "failed to receive client\n";
+  }
+  std::cout << "connected to client!\n";
+
+
+  send(client_fd_, k_header.c_str(), k_header.size(), 0);
   status_ = true;
 }
 
-void Streamer::Listen(){
-  // TODO make thread safe
-  while (true){
-    int client_fd = accept(server_fd_, (struct sockaddr*)&address_, &address_length_);
-    send(client_fd, k_header.c_str(), k_header.size(), 0);
-    if (client_fd >= 0){
-      client_fd_.push_back(client_fd);
-    }
-  }
-}
+// void Streamer::Listen(){
+//   // TODO make thread safe
+//   while (true){
+//     int client_fd = accept(server_fd_, (struct sockaddr*)&address_, &address_length_);
+//     send(client_fd, k_header.c_str(), k_header.size(), 0);
+//     if (client_fd >= 0){
+//       if (verbose_){
+//         std::cout << "got new client\n";
+//       }
+//       client_fd_.push_back(client_fd);
+//     }
+//   }
+// }
 
 void Streamer::writeFrame(cv::Mat &mat){
-  cv::Mat compressed;
-  cv::resize(mat, compressed, cv::Size(480, 480), 0, 0, cv::INTER_AREA);
-  cv::cvtColor(compressed, compressed, cv::COLOR_BGR2GRAY);
+    cv::Mat compressed;
+    cv::resize(mat, compressed, cv::Size(480, 480), 0, 0, cv::INTER_AREA);
+    cv::cvtColor(compressed, compressed, cv::COLOR_BGR2GRAY);
 
-  std::vector<uchar> buf;
-  cv::imencode(".jpg", compressed, buf);
+    std::vector<uchar> buf;
+    cv::imencode(".jpg", compressed, buf);
 
-  std::string part = 
+    std::string part = 
       "--frame\r\n"
       "Content-Type: image/jpeg\r\n"
       "Content-Length: " + std::to_string(buf.size()) + "\r\n\r\n";
-  for (int client_fd : client_fd_){
-    send(client_fd, part.c_str(), part.size(), 0);
-    send(client_fd, reinterpret_cast<char*>(buf.data()), buf.size(), 0);
-    send(client_fd, "\r\n", 2, 0);
-  }
+    send(client_fd_, part.c_str(), part.size(), 0);
+    send(client_fd_, reinterpret_cast<char*>(buf.data()), buf.size(), 0);
+    send(client_fd_, "\r\n", 2, 0);
 }
 }
 
