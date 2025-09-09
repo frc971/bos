10175ead2d866e622cee2ed1dec0ed86d0bc808c
Your Name <you@example.com>
2025-09-09 04:35:10 +0000

    make blank test
    Signed-off-by: Your Name <you@example.com>
    
    Former-commit-id: 64cd79ec4934fdb0e2ce1c961eb2ef7d62db2750

--- main/test.cc
+++ main/test.cc
@@ -16,8 +16,4 @@
 
 using json = nlohmann::json;
 
-int main() {
-  frc::AprilTagFieldLayout layout(frc::AprilTagFieldLayout::LoadField(
-      frc::AprilTagField::k2025ReefscapeAndyMark));
-  std::cout << layout.GetTagPose(7)->Rotation().Z().value();
-}
+int main() {}
