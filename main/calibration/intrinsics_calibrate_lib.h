dd38facf0126858a4c3832719284b51a6971c840
Your Name <you@example.com>
2025-09-11 21:30:40 +0000

    change to board we have at the lab
    Signed-off-by: Your Name <you@example.com>

--- main/calibration/intrinsics_calibrate_lib.h
+++ main/calibration/intrinsics_calibrate_lib.h
@@ -19,14 +19,14 @@ typedef struct DetectionResult {
 } detection_result_t;
 
 cv::aruco::CharucoDetector CreateDetector(cv::aruco::Dictionary dictionary,
-                                          int squares_x = 5, int squares_y = 7,
-                                          float squares_length = 0.04,
+                                          int squares_x = 12, int squares_y = 9,
+                                          float squares_length = 0.06,
                                           float pixel_per_square = 128,
                                           float marker_length = 0.02);
 
 json intrisincs_to_json(cv::Mat cameraMatrix, cv::Mat distCoeffs);
-cv::Mat GenerateBoard(cv::aruco::CharucoBoard board, int squares_x = 5,
-                      int squares_y = 7, float pixel_per_square = 128,
+cv::Mat GenerateBoard(cv::aruco::CharucoBoard board, int squares_x = 12,
+                      int squares_y = 9, float pixel_per_square = 128,
                       int margin_squares = 0);
 
 detection_result_t DetectCharucoBoard(cv::Mat& frame,
