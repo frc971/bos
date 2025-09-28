c89fae8d542627f17fcb7cbf53ff757ff62a4e10
yasen5 <carrotmercinary@gmail.com>
2025-09-28 06:11:02 +0000

    UNTESTED add file to export binary model to engine

--- main/yolo/realsense_yolo_test.cc
+++ main/yolo/realsense_yolo_test.cc
@@ -3,12 +3,12 @@
 #include "yolo/yolo.h"
 
 int main() {
-  yolo::Yolo model("yolo11n.pt");
+  yolo::Yolo model("/bos/main/yolo/yolo11n.pt");
   camera::RealSenseCamera rs_camera;
   cv::Mat mat;
   rs_camera.getFrame(mat);
   const std::vector<float> maybe_softmax_results = model.RunModel(mat);
-  for (const float& maybe_softmax_val : model.RunModel(mat)) {
+  for (const float& maybe_softmax_val : maybe_softmax_results) {
     std::cout << maybe_softmax_val << std::endl;
   }
 }
