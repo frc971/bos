719e3772f912d0d56e03105689616f2ac13180ea
Charlie Huang <charliehuang09@gmail.com>
2025-10-11 20:54:47 +0000

    fix formating
    Signed-off-by: Charlie Huang <charliehuang09@gmail.com>

--- src/localization/position_sender.cc
+++ src/localization/position_sender.cc
@@ -19,85 +19,16 @@ constexpr double RadianToDegree(double radian) {
 PositionSender::PositionSender(bool verbose)
     : instance_(nt::NetworkTableInstance::GetDefault()), verbose_(verbose) {
   std::shared_ptr<nt::NetworkTable> table =
-      instance_.GetTable("orin/pose_estimate");
-
-  // nt::DoubleTopic translation_x_topic = table->GetDoubleTopic("translation_x");
-  // nt::DoubleTopic translation_y_topic = table->GetDoubleTopic("translation_y");
-  //
-  // nt::DoubleTopic rotation_topic = table->GetDoubleTopic("rotation");
-  //
-  // nt::DoubleTopic translation_x_varience_topic =
-  //     table->GetDoubleTopic("translation_x_varience");
-  // nt::DoubleTopic translation_y_varience_topic =
-  //     table->GetDoubleTopic("translation_y_varience");
-  //
-  // nt::DoubleTopic rotation_varience_topic =
-  //     table->GetDoubleTopic("rotation_varience");
+      instance_.GetTable("Orin/PoseEstimate");
 
   nt::StructTopic<frc::Pose2d> pose_topic =
-      table->GetStructTopic<frc::Pose2d>("pose");
-
-  // translation_x_publisher_ =
-  //     translation_x_topic.Publish({.sendAll = true, .keepDuplicates = true});
-  // translation_y_publisher_ =
-  //     translation_y_topic.Publish({.sendAll = true, .keepDuplicates = true});
-  //
-  // rotation_publisher_ =
-  //     rotation_topic.Publish({.sendAll = true, .keepDuplicates = true});
-  //
-  // translation_x_varience_publisher_ = translation_x_varience_topic.Publish(
-  //     {.sendAll = true, .keepDuplicates = true});
-  // translation_y_varience_publisher_ = translation_y_varience_topic.Publish(
-  //     {.sendAll = true, .keepDuplicates = true});
-  //
-  // rotation_varience_publisher_ = rotation_varience_topic.Publish(
-  //     {.sendAll = true, .keepDuplicates = true});
-  //
-  // nt::DoubleTopic timestamp_topic = table->GetDoubleTopic("timestamp");
-  // timestamp_publisher_ =
-  //     timestamp_topic.Publish({.sendAll = true, .keepDuplicates = true});
-
+      table->GetStructTopic<frc::Pose2d>("Pose");
   pose_publisher_ = pose_topic.Publish();
 
   nt::DoubleArrayTopic tag_estimation_topic =
-      table->GetDoubleArrayTopic("tag_estimation");
+      table->GetDoubleArrayTopic("TagEstimation");
   tag_estimation_publisher_ =
       tag_estimation_topic.Publish({.sendAll = true, .keepDuplicates = true});
-}
-
-void PositionSender::Send(pose2d_t position_estimates, pose2d_t varience) {
-  if (mutex_.try_lock()) {
-    // translation_x_publisher_.Set(position_estimates.x);
-    // translation_y_publisher_.Set(position_estimates.y);
-    // rotation_publisher_.Set(position_estimates.rotation);
-    //
-    // translation_x_varience_publisher_.Set(varience.x);
-    // translation_y_varience_publisher_.Set(varience.y);
-    // rotation_varience_publisher_.Set(varience.rotation);
-    // pose_publisher_.Set(
-    //     frc::Pose2d(units::meter_t{position_estimates.x},
-    //                 units::meter_t{position_estimates.y},
-    //                 units::radian_t{position_estimates.rotation}));
-    // timestamp_publisher_.Set(frc::Timer::GetFPGATimestamp().value() +
-    //                          instance_.GetServerTimeOffset().value() * 1000000);
-
-    mutex_.unlock();
-  }
-  if (verbose_) {
-    std::cout << "Position Sender: "
-              << "\n";
-    std::cout << "Translation: "
-              << "\n";
-    std::cout << position_estimates.x << "\n";
-    std::cout << position_estimates.y << "\n";
-    std::cout << RadianToDegree(position_estimates.rotation) << "\n";
-
-    std::cout << "Varience: "
-              << "\n";
-    std::cout << varience.x << "\n";
-    std::cout << varience.y << "\n";
-    std::cout << varience.rotation << "\n";
-  }
 }
 
 void PositionSender::Send(
