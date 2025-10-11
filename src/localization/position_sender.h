f4e8c7564b816cf6e094dbae37d99a8617b15c8e
Charlie Huang <charliehuang09@gmail.com>
2025-10-11 04:59:00 +0000

    send seperate estiamtes
    to be tested
    
    Signed-off-by: Charlie Huang <charliehuang09@gmail.com>

--- src/localization/position_sender.h
+++ src/localization/position_sender.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <networktables/BooleanTopic.h>
+#include <networktables/DoubleArrayTopic.h>
 #include <networktables/DoubleTopic.h>
 #include <networktables/NetworkTable.h>
 #include <networktables/NetworkTableInstance.h>
@@ -13,6 +14,7 @@ class PositionSender {
  public:
   PositionSender(bool verbose = false);
   void Send(pose2d_t position_estimate, pose2d_t varience);
+  void Send(std::vector<localization::tag_detection_t> detections);
 
  private:
   nt::NetworkTableInstance instance_;
@@ -30,6 +32,8 @@ class PositionSender {
   nt::DoublePublisher timestamp_publisher_;
 
   nt::StructPublisher<frc::Pose2d> pose_publisher_;
+
+  nt::DoubleArrayPublisher tag_estimation_publisher_;
 
   std::mutex mutex_;
   bool verbose_;
