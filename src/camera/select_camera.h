f4b129a27de4f923428973490e5f4cd3cdf35108
starax-bit <rajanhensley@icloud.com>
2025-10-21 18:52:25 +0000

    Implement SelectCamera
    Signed-off-by: starax-bit <rajanhensley@icloud.com>

--- src/camera/select_camera.h
+++ src/camera/select_camera.h
@@ -1,7 +1,44 @@
 #pragma once
 #include "src/camera/cv_camera.h"
+#include <iostream>
 namespace camera {
 
-CVCamera SelectCamera();
 
+// Asks Users for input and selects camera based on that.
+CVCamera SelectCamera();
+    std::cout << "Which input do you want to use; usb or mipi: ";
+    string camType;
+    std::cin >> camType;
+
+    if (camType == "usb") {
+        std::cout << "Which camera number; 1-4: ";
+        int camNum;
+        std::cin >> camNum;
+
+        switch (camNum) {
+            case 1:
+                return std::make_unique<camera::CVCamera>(cv::VideoCapture("/dev/video0"));
+
+            case 2:
+                return std::make_unique<camera::CVCamera>(cv::VideoCapture("/dev/video2"));
+
+            case 3:
+                return std::make_unique<camera::CVCamera>(cv::VideoCapture("/dev/video4"));
+
+            case 4:
+                return std::make_unique<camera::CVCamera>(cv::VideoCapture("/dev/video6"));
+
+            default:
+                std::cout << "INVALID INPUT: must be a number from 1 to 4.";
+                SelectCamera();
+        }
+        
+
+    } else if (camType == "mipi") {
+
+    } else {
+        std::cout << "INVALID INPUT: must be lowercase";
+        SelectCamera();
+
+    }
 }
