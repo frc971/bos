964562fa107e5d826e29fe5e3f69fd779a2e46d7
Charlie Huang <charliehuang09@gmail.com>
2025-10-25 21:26:16 +0000

    wip
    Signed-off-by: Charlie Huang <charliehuang09@gmail.com>

--- src/camera/camera_constants.h
+++ src/camera/camera_constants.h
@@ -4,38 +4,51 @@
 namespace camera {
 
 typedef struct CameraConstant {
-    std::string a;
-    CameraConstant(std::string idk) : a(idk){}
+  std::string pipeline;
+  std::string intrinsics_path;
+  std::string extrinsics_path;
 } camera_constant_t;
 
 enum CamConstant {
-imx296_1 = 0,
-imx296_2 = 1,
+  USB1 = 0,
+  USB2 = 1,
 };
 
-camera_constant_t a[2] = {[CamConstant::imx296_1]={"1"}, [1]={"d"}};
+inline const camera_constant_t a[2] = {
+    [CamConstant::USB1] = camera_constant_t{.pipeline = "1",
+                                            .intrinsics_path = "",
+                                            .extrinsics_path = ""},
+    [CamConstant::USB2] = camera_constant_t{.pipeline = "1", .intrinsics_path = "", .extrinsics_path = ""}};
 
+const std::string usb_camera0_intrinsics =
+    "constants/usb_camera0_intrinsics.json";
+const std::string usb_camera0_extrinsics =
+    "constants/usb_camera0_extrinsics.json";
 
-const std::string usb_camera0_intrinsics = "constants/usb_camera0_intrinsics.json";
-const std::string usb_camera0_extrinsics = "constants/usb_camera0_extrinsics.json";
+const std::string imx296_camera1_intrinsics =
+    "constants/imx296_camera1_intrinsics.json";
+const std::string imx296_camera1_extrinsics =
+    "constants/imx296_camera1_extrinsics.json";
 
-const std::string imx296_camera1_intrinsics = "constants/imx296_camera1_intrinsics.json";
-const std::string imx296_camera1_extrinsics = "constants/imx296_camera1_extrinsics.json";
+const std::string imx296_camera2_intrinsics =
+    "constants/imx296_camera2_intrinsics.json";
+const std::string imx296_camera2_extrinsics =
+    "constants/imx296_camera2_extrinsics.json";
 
-const std::string imx296_camera2_intrinsics = "constants/imx296_camera2_intrinsics.json";
-const std::string imx296_camera2_extrinsics = "constants/imx296_camera2_extrinsics.json";
+const std::string usb_camera1_intrinsics =
+    "constants/usb_camera1_intrinsics.json";
+const std::string usb_camera1_extrinsics =
+    "constants/usb_camera1_extrinsics.json";
 
-const std::string usb_camera1_intrinsics = "constants/usb_camera1_intrinsics.json";
-const std::string usb_camera1_extrinsics = "constants/usb_camera1_extrinsics.json";
-
-const std::string usb_camera2_intrinsics = "constants/usb_camera2_intrinsics.json";
-const std::string usb_camera2_extrinsics = "constants/usb_camera2_extrinsics.json";
+const std::string usb_camera2_intrinsics =
+    "constants/usb_camera2_intrinsics.json";
+const std::string usb_camera2_extrinsics =
+    "constants/usb_camera2_extrinsics.json";
 
 const std::string usb_camera0 =
-"/dev/v4l/by-path/platform-3610000.usb-usb-0:2.1:1.0-video-index0";
+    "/dev/v4l/by-path/platform-3610000.usb-usb-0:2.1:1.0-video-index0";
 
 const std::string usb_camera1 =
-"/dev/v4l/by-path/platform-3610000.usb-usb-0:2.4:1.0-video-index0";
+    "/dev/v4l/by-path/platform-3610000.usb-usb-0:2.4:1.0-video-index0";
 
-};
-
+};  // namespace camera
