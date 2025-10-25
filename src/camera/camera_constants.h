6537b1e1bd6a742128f00b7ac9be80d2b5590d29
starax-bit <rajanhensley@icloud.com>
2025-10-25 13:01:45 -0700

    Implement SelectCamera (#35)

--- src/camera/camera_constants.h
+++ src/camera/camera_constants.h
@@ -3,9 +3,15 @@
 
 namespace camera {
 
-const std::string camera1_intrinsics = "constants/camera1_intrinsics.json";
-const std::string camera1_extrinsics = "constants/camera1_extrinsics.json";
+const std::string imx296_camera1_intrinsics = "constants/imx296_camera1_intrinsics.json";
+const std::string imx296_camera1_extrinsics = "constants/imx296_camera1_extrinsics.json";
 
-const std::string camera2_intrinsics = "constants/camera2_intrinsics.json";
-const std::string camera2_extrinsics = "constants/camera2_extrinsics.json";
+const std::string imx296_camera2_intrinsics = "constants/imx296_camera2_intrinsics.json";
+const std::string imx296_camera2_extrinsics = "constants/imx296_camera2_extrinsics.json";
+
+const std::string usb_camera1_intrinsics = "constants/usb_camera1_intrinsics.json";
+const std::string usb_camera1_extrinsics = "constants/usb_camera1_extrinsics.json";
+
+const std::string usb_camera2_intrinsics = "constants/usb_camera2_intrinsics.json";
+const std::string usb_camera2_extrinsics = "constants/usb_camera2_extrinsics.json";
 }  // namespace camera
--- src/camera/select_camera.h
+++ src/camera/select_camera.h
@@ -1,7 +1,6 @@
 #pragma once
 #include "src/camera/cv_camera.h"
+
 namespace camera {
-
-CVCamera SelectCamera();
-
+    CVCamera SelectCamera(); 
 }
