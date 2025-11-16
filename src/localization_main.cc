1f24d1ace9db43b334398659afb78c961d15406b
Charlie Huang <charliehuang09@gmail.com>
2025-11-16 00:29:14 +0000

    add name
    Signed-off-by: Charlie Huang <charliehuang09@gmail.com>

--- src/localization_main.cc
+++ src/localization_main.cc
@@ -47,7 +47,8 @@ json read_extrinsics(std::string path) {
   return extrinsics;
 }
 
-void run_estimator(const int frame_width, const int frame_height,
+void run_estimator(std::string name, const int frame_width,
+                   const int frame_height,
                    std::unique_ptr<camera::CVCamera> cap, json intrinsics,
                    json extrinsics,
                    localization::PositionSender& position_sender, int port) {
@@ -55,8 +56,7 @@ void run_estimator(const int frame_width, const int frame_height,
   localization::TagEstimator tag_estimator(frame_width, frame_height,
                                            intrinsics, extrinsics);
 
-  camera::CscoreStreamer streamer("localization_main", port, 30, 480, 480,
-                                  false);
+  camera::CscoreStreamer streamer(name, port, 30, 480, 480, false);
 
   cv::Mat frame;
   while (true) {
@@ -75,7 +75,7 @@ int main() {
   localization::PositionSender position_sender(true);
 
   std::thread usb0_thread(
-      run_estimator, 640, 480,
+      run_estimator, "back_left", 640, 480,
       std::make_unique<camera::CVCamera>(cv::VideoCapture(
           camera::camera_constants[camera::Camera::USB0].pipeline)),
       read_intrinsics(
@@ -85,7 +85,7 @@ int main() {
       std::ref(position_sender), 4971);
 
   std::thread usb1_thread(
-      run_estimator, 1280, 720,
+      run_estimator, "back_right", 1280, 720,
       std::make_unique<camera::CVCamera>(cv::VideoCapture(
           camera::camera_constants[camera::Camera::USB1].pipeline)),
       read_intrinsics(
