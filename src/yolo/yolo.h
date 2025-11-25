28af08f35aea9a949cffbd661fa25b063f662bcd
yasen5 <yasengiuli@gmail.com>
2025-11-25 00:05:26 +0000

    Add main yolo stuff
    Former-commit-id: cd55710643e24d8374756a81f8c696adc2ea85b6

--- src/yolo/yolo.h
+++ src/yolo/yolo.h
@@ -11,14 +11,26 @@ class Yolo {
  public:
   Yolo(std::string model_path, bool verbose = false);
   ~Yolo();
-  std::vector<float> RunModel(cv::Mat frame);
+  std::vector<float> RunModel(const cv::Mat& frame);
+  std::vector<float> Postprocess(const cv::Mat& mat,
+                                 std::vector<cv::Rect>& bboxes,
+                                 std::vector<float>& confidences,
+                                 std::vector<int>& class_ids);
+  static void DrawDetections(cv::Mat& img, const std::vector<cv::Rect>& boxes,
+                             const std::vector<int>& class_ids,
+                             const std::vector<float>& confidences,
+                             const std::vector<std::string>& class_names);
 
  private:
+  static void PreprocessImage(const cv::Mat& frame, float* gpu_input,
+                              const nvinfer1::Dims64& dims);
   nvinfer1::IRuntime* runtime_;
   nvinfer1::ICudaEngine* engine_;
   nvinfer1::IExecutionContext* context_;
   cudaStream_t inferenceCudaStream_;
-  void* output_buffer_;
+  nvinfer1::Dims64 input_dims_;
+  float* input_buffer_;
+  float* output_buffer_;
   size_t output_size_;
   bool verbose_;
 };
---
+++ src/yolo/yolo_test.cc
@@ -0,0 +1,49 @@
+#include <filesystem>
+#include <iostream>
+#include <opencv2/opencv.hpp>
+#include <string>
+#include "src/camera/cv_camera.h"
+#include "src/camera/select_camera.h"
+#include "src/yolo/yolo.h"
+
+int main() {
+  std::filesystem::path modelPath = "/bos/src/yolo/model/ninthYOLO.engine";
+  std::cout << "Importing model from " << modelPath << std::endl;
+  std::cout << "File actually exists: " << std::filesystem::exists(modelPath)
+            << std::endl;
+  yolo::Yolo model(modelPath);
+  camera::CVCamera camera = camera::CVCamera(cv::VideoCapture(camera::SelectCamera()));
+  const int max_detections = 6;
+  std::vector<cv::Rect> bboxes(max_detections);
+  std::vector<float> confidences(max_detections);
+  std::vector<int> class_ids(max_detections);
+  std::vector<std::string> class_names = {"Algae", "ALGAE", "Coral", "CORAL"};
+  const bool test_collected = false;
+  cv::Mat color;
+  if (test_collected) {
+    for (const auto& entry : std::filesystem::directory_iterator(
+             "/home/nvidia/Documents/collected_imgs")) {
+      cv::Mat mat = cv::imread(entry.path().string());
+      model.Postprocess(mat, bboxes, confidences, class_ids);
+      yolo::Yolo::DrawDetections(mat, bboxes, class_ids, confidences,
+                                 class_names);
+      cv::imshow("Test detections", mat);
+      cv::waitKey(0);
+      cv::destroyAllWindows();
+    }
+  } else {
+    while (true) {
+      camera.GetFrame(color);
+      if (color.empty()) {
+        std::cout << "Couldn't fetch frame properly" << std::endl;
+        return 1;
+      }
+      model.Postprocess(color, bboxes, confidences, class_ids);
+      yolo::Yolo::DrawDetections(color, bboxes, class_ids, confidences,
+                                 class_names);
+      cv::imshow("Test detections", color);
+      cv::waitKey(0);
+      cv::destroyAllWindows();
+    }
+  }
+}
