a063205ef12dc78597e6a1c7e9b766f8c2d70524
Charlie Huang <charliehuang09@gmail.com>
2025-11-28 02:29:07 +0000

    wip
    Signed-off-by: Charlie Huang <charliehuang09@gmail.com>

--- src/camera/camera_source.cc
+++ src/camera/camera_source.cc
@@ -1,13 +1,38 @@
 #include "camera_source.h"
+#include <wpilibc/frc/Timer.h>
+#include <algorithm>
+#include <cstdlib>
 #include <memory>
 
 namespace camera {
-CameraSource::CameraSource(std::unique_ptr<ICamera> camera)
-    : camera_(std::move(camera)), buffer_(250) {
-  thread_ = std::thread([this]() {
+
+CameraSource::CameraSource(int width, int height, int channels, int image_type,
+                           std::unique_ptr<ICamera> camera, int length)
+    : width_(width),
+      height_(height),
+      channels_(channels),
+      image_type_(image_type),
+      image_size_(width * height * channels * sizeof(uint8_t)),
+      camera_(std::move(camera)),
+      length_(length),
+      head_(-1) {
+  buffer_ = static_cast<uint8_t*>(malloc(image_size_ * length));
+  thread_ = std::thread([this] {
     while (true) {
-      buffer_.push_back(camera->GetFrame(cv::Mat & mat))
+      cv::Mat mat(height_, width_, image_type_,
+                  buffer_ + image_size_ * (head_ + 1));
+      camera_->GetFrame(mat);
+      timestamp_[head_ + 1] = frc::Timer::GetFPGATimestamp().to<double>();
+
+      head_ += 1;
     }
   });
-};
+}
+
+frame_t CameraSource::Get() {
+  cv::Mat mat(height_, width_, image_type_, buffer_ + image_size_ * head_);
+  double timestamp = timestamp_[head_];
+  return frame_t{.mat = mat, .timestamp = timestamp};
+}
+
 }  // namespace camera
--- src/camera/camera_source.h
+++ src/camera/camera_source.h
@@ -1,25 +1,34 @@
 #pragma once
 
+#include <opencv2/core/hal/interface.h>
 #include <opencv2/core/mat.hpp>
 #include <string>
 #include <thread>
 #include "src/camera/camera.h"
-#include "third_party/circular_buffer/circular_buffer.h"
 namespace camera {
 
 typedef struct Frame {
-  cv::Mat& mat;
+  cv::Mat mat;
   double timestamp;
 } frame_t;
 
 class CameraSource {
  public:
-  CameraSource(std::unique_ptr<ICamera> camera);
+  CameraSource(int width, int height, int channels, int image_type,
+               std::unique_ptr<ICamera> camera, int length);
   frame_t Get();
 
  private:
+  int width_;
+  int height_;
+  int channels_;
+  int image_type_;
+  size_t image_size_;
   std::unique_ptr<ICamera> camera_;
-  CircularBuffer<frame_t> buffer_;
+  int length_;
+  int head_;
+  uint8_t* buffer_;
+  double* timestamp_;
   std::thread thread_;
 };
 
