71200d4212358e07a2e308174c160c98c7e7ea53
Charlie Huang <charliehuang09@gmail.com>
2025-12-03 17:56:25 +0000

    format
    Signed-off-by: Charlie Huang <charliehuang09@gmail.com>

--- src/test/yolo_test.cc
+++ src/test/yolo_test.cc
@@ -1,10 +1,10 @@
+#include "src/yolo/yolo.h"
 #include <filesystem>
 #include <iostream>
 #include <opencv2/opencv.hpp>
 #include <string>
 #include "src/camera/cv_camera.h"
 #include "src/camera/select_camera.h"
-#include "src/yolo/yolo.h"
 
 int main() {
   std::filesystem::path modelPath = "/bos/src/yolo/model/ninthYOLO.engine";
@@ -12,12 +12,15 @@ int main() {
   std::cout << "File actually exists: " << std::filesystem::exists(modelPath)
             << std::endl;
   yolo::Yolo model(modelPath);
-  camera::CVCamera camera = camera::CVCamera(cv::VideoCapture(camera::SelectCamera()));
+  camera::CVCamera camera =
+      camera::CVCamera(cv::VideoCapture(camera::SelectCamera()));
   const int max_detections = 6;
   std::vector<cv::Rect> bboxes(max_detections);
   std::vector<float> confidences(max_detections);
   std::vector<int> class_ids(max_detections);
-  std::vector<std::string> class_names = {"Algae", "ALGAE", "Coral", "CORAL"}; // Chopped because I screwed up on the dataset, and technically the model outputs "CORAL", "coral", "ALGAE" or "algae"
+  std::vector<std::string> class_names = {
+      "Algae", "ALGAE", "Coral",
+      "CORAL"};  // Chopped because I screwed up on the dataset, and technically the model outputs "CORAL", "coral", "ALGAE" or "algae"
   const bool test_collected = false;
   cv::Mat color;
   if (test_collected) {
