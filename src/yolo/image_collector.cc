92d6d79974e6ff23afc7e03a2aa69d2964843128
yasen5 <yasengiuli@gmail.com>
2025-12-06 04:41:05 +0000

    Make image collector dependent on frc time
    Former-commit-id: f1d10f081bf020de364e2be6b4588ed7b284695d

--- src/yolo/image_collector.cc
+++ src/yolo/image_collector.cc
@@ -1,27 +1,21 @@
-#include <chrono>
-#include <ctime>
+#include <wpilibc/frc/Timer.h>
 #include <filesystem>
 #include <opencv2/opencv.hpp>
 #include <thread>
 #include "src/camera/select_camera.h"
+#include <chrono>
+#include <string>
 
 int main() {
-  std::time_t now = std::time(nullptr);
-  std::tm* localTime = std::localtime(&now);
-  int i = 0;
-  const std::string img_dir = "/bos/logs/collected_imgs/";
+  std::string img_dir = "/bos/logs/collected_imgs/";
   std::filesystem::create_directories(img_dir);
-  const std::string name_start = "frame_" + std::to_string(localTime->tm_mday) +
-                                 "_" + std::to_string(localTime->tm_hour) + "_";
   camera::CVCamera camera =
       camera::CVCamera(cv::VideoCapture(camera::SelectCamera()));
   while (true) {
     cv::Mat frame;
     camera.GetFrame(frame);
-    cv::imwrite(img_dir + name_start + std::to_string(localTime->tm_min) + "_" +
-                    std::to_string(i) + ".bmp",
+    cv::imwrite(img_dir + frc::Timer::GetFPGATimestamp()) + ".bmp",
                 frame);
-    i++;
     std::this_thread::sleep_for(std::chrono::milliseconds(300));
   }
 }
