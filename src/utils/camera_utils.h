5899dbda014c7ca8ce3ea3123b0e24950d8651a7
yasen5 <carrotmercinary@gmail.com>
2025-12-05 21:13:39 -0800

    Add camera utils
    Former-commit-id: 690ea8f813b9708ab7fe316dd1563f79e27c525f

---
+++ src/utils/camera_utils.h
@@ -0,0 +1,7 @@
+#include <string>
+#include "nlohmann/json.hpp"
+
+namespace utils {
+nlohmann::json read_intrinsics(std::string path);
+nlohmann::json read_extrinsics(std::string path);
+}  // namespace utils
