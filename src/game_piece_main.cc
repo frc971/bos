5d9dd5b7a1f8778b20de0f7f17d8f1fb008dcd1b
yasen5 <carrotmercinary@gmail.com>
2025-12-20 10:56:13 -0800

    Remove unnecessary rs function and maybe fix angles
    Former-commit-id: afd94dd2b0dc8e9392221a8e10a69b776bb381b1

--- src/game_piece_main.cc
+++ src/game_piece_main.cc
@@ -58,9 +58,7 @@ void run_gamepiece_detect(yolo::Yolo& model,
   const float focal_length_vertical =
       intrinsics["fy"].get<float>();  // needs to be meters
   const float focal_length_horizontal = intrinsics["fx"].get<float>();
-  const float cam_pitch =
-      -(float)extrinsics
-          ["rotation_y"];  // negative because if the camera is tilted up, phi should be smaller than the theta read by camera
+  const float cam_pitch = extrinsics["rotation_y"];
   const frc::Pose3d cam_pose{
       frc::Translation3d{
           units::meter_t{extrinsics["translation_x"].get<float>()},
@@ -125,106 +123,6 @@ void run_gamepiece_detect(yolo::Yolo& model,
   }
 }
 
-void run_gamepiece_detect_realsense(yolo::Yolo& model,
-                                    std::unique_ptr<camera::RealSenseCamera> rs,
-                                    nt::StructTopic<frc::Pose2d>& coral_topic,
-                                    nt::StructTopic<frc::Pose2d>& algae_topic,
-                                    nlohmann::json intrinsics,
-                                    nlohmann::json extrinsics) {
-  nt::StructPublisher<frc::Pose2d> coral_pub = coral_topic.Publish();
-  nt::StructPublisher<frc::Pose2d> algae_pub = algae_topic.Publish();
-  cv::Mat color;
-  cv::Mat depth;
-  std::vector<cv::Rect> bboxes(MAX_DETECTIONS);
-  std::vector<float> confidences(MAX_DETECTIONS);
-  std::vector<int> class_ids(MAX_DETECTIONS);
-  const frc::Pose3d cam_pose{
-      frc::Translation3d{
-          units::meter_t{extrinsics["translation_x"].get<float>()},
-          units::meter_t{extrinsics["translation_y"].get<float>()},
-          units::meter_t{extrinsics["translation_z"].get<float>()}},
-      frc::Rotation3d{
-          units::radian_t{extrinsics["rotation_x"].get<float>()},
-          units::radian_t{extrinsics["rotation_y"].get<float>()},
-          units::radian_t{(float)extrinsics["rotation_z"].get<float>()}}};
-  const float cam_cx = intrinsics["cx"];
-  const float cam_cy = intrinsics["cy"];
-  const float focal_length_horizontal = intrinsics["fx"];
-  frc::Transform3d target_pose_cam_relative;
-  const float focal_length_vertical = intrinsics["fy"];
-  frc::Pose3d target_pose_robot_relative;
-  while (true) {
-    rs->GetFrame(color, depth);
-    mutex.lock();
-    model.Postprocess(color.rows, color.cols, model.RunModel(color), bboxes,
-                      confidences, class_ids);
-    mutex.unlock();
-    if (color.empty()) {
-      std::cout << "Couldn't fetch frame properly" << std::endl;
-      continue;
-    }
-    for (size_t i = 0; i < MAX_DETECTIONS; i++) {
-      if (bboxes[i].empty()) {
-        if (i == 0) {
-          std::cout << "No detections" << std::endl;
-        }
-        break;
-      }
-      const int c_y = bboxes[i].y + bboxes[i].height / 2;
-      const int c_x = bboxes[i].x + bboxes[i].width / 2;
-      float distance = depth.at<float>(c_y, c_x);
-      if (distance == 0) {
-        for (int j = 0; distance == 0 && j < bboxes[i].width / 2;
-             j += bboxes[i].width / 10) {
-          distance = depth.at<float>(c_y, c_x + j);
-          if (distance == 0)
-            distance = depth.at<float>(c_y, c_x - j);
-        }
-        for (int j = 0; distance == 0 && j < bboxes[i].height / 2;
-             j += bboxes[i].height / 10) {
-          if (distance == 0) {
-            distance = depth.at<float>(c_y + j, c_x);
-            distance = depth.at<float>(c_y - j, c_x);
-          }
-        }
-        if (distance == 0) {
-          std::cout << "Couldn't find reasonable distance value after scan, "
-                       "discarding this one"
-                    << std::endl;
-          continue;
-        }
-      }
-
-      const std::string& class_name = class_names[class_ids[i]];
-
-      std::cout << "Detected a " << class_name << " " << distance
-                << " meters away" << std::endl;
-
-      const float cam_relative_pitch =
-          atan2(c_y - cam_cy, focal_length_vertical);
-      const float cam_relative_yaw =
-          atan2(c_x - cam_cx, focal_length_horizontal);
-      target_pose_cam_relative = {
-          frc::Translation3d{
-              units::meter_t{distance * cos(cam_relative_pitch) *
-                             cos(cam_relative_yaw)},
-              units::meter_t{distance * cos(cam_relative_pitch) *
-                             sin(cam_relative_yaw)},
-              units::meter_t{distance * sin(cam_relative_pitch)}},
-          frc::Rotation3d{0_rad, units::radian_t{cam_relative_pitch},
-                          units::radian_t{-cam_relative_yaw}}};
-      target_pose_robot_relative =
-          cam_pose.TransformBy(target_pose_cam_relative);
-
-      if (class_name == "coral") {
-        coral_pub.Set(target_pose_robot_relative.ToPose2d());
-      } else {
-        algae_pub.Set(target_pose_robot_relative.ToPose2d());
-      }
-    }
-  }
-}
-
 int main() {
   std::cout << std::fixed << std::setprecision(2);
   std::cout << "Starting gamepiece main" << std::endl;
@@ -243,24 +141,11 @@ int main() {
       algae_table->GetStructTopic<frc::Pose2d>("Pose");
 
   std::vector<std::thread> camera_threads;
-  const bool using_rs = false;
-  if (using_rs) {
-    // purely for testing bc mechanical is mean to realsense :(
-    camera_threads.emplace_back(
-        run_gamepiece_detect_realsense, std::ref(color_model),
-        std::make_unique<camera::RealSenseCamera>(), std::ref(coral_topic),
-        std::ref(algae_topic),
-        utils::read_intrinsics(
-            camera::camera_constants[camera::Camera::USB0].intrinsics_path),
-        utils::read_extrinsics(
-            camera::camera_constants[camera::Camera::USB0].extrinsics_path));
-  } else {
-    camera_threads.emplace_back(
-        run_gamepiece_detect, std::ref(color_model),
-        std::make_unique<camera::RealSenseCamera>(), std::ref(coral_topic),
-        std::ref(algae_topic),
-        utils::read_intrinsics("/bos/constants/realsense_intrinsics.json"),
-        utils::read_extrinsics("/bos/constants/realsense_extrinsics.json"));
-  }
+  camera_threads.emplace_back(
+      run_gamepiece_detect, std::ref(color_model),
+      std::make_unique<camera::RealSenseCamera>(), std::ref(coral_topic),
+      std::ref(algae_topic),
+      utils::read_intrinsics("/bos/constants/realsense_intrinsics.json"),
+      utils::read_extrinsics("/bos/constants/realsense_extrinsics.json"));
   camera_threads[0].join();
 }
