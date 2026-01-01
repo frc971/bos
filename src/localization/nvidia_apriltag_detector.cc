86c3df5fc36e759e7ca199e3d458626489dd65b4
charliehuang09 <41605449+charliehuang09@users.noreply.github.com>
2025-12-31 17:35:17 -0800

    Add nvidia apriltag backend (#64)
    * add vpi library
    
    Signed-off-by: Charlie Huang <charliehuang09@gmail.com>
    
    * add vpi to cmakelists
    
    Signed-off-by: Charlie Huang <charliehuang09@gmail.com>
    
    * make tag_estimator use unique ptr instead
    
    Signed-off-by: Charlie Huang <charliehuang09@gmail.com>
    
    * wip refactor
    
    Signed-off-by: Charlie Huang <charliehuang09@gmail.com>
    
    * add refactored tag estimator to stress test
    
    Signed-off-by: Charlie Huang <charliehuang09@gmail.com>
    
    * refactor run localization
    
    Signed-off-by: Charlie Huang <charliehuang09@gmail.com>
    
    * remove tag_estimator
    
    Signed-off-by: Charlie Huang <charliehuang09@gmail.com>
    
    * add nvidia gpu detection test
    
    Signed-off-by: Charlie Huang <charliehuang09@gmail.com>
    
    * wip
    
    Signed-off-by: Charlie Huang <charliehuang09@gmail.com>
    
    * address yassen comments
    
    Signed-off-by: Charlie Huang <charliehuang09@gmail.com>
    
    * refactor tag_detection_t to use frc::Pose3d
    
    Signed-off-by: Charlie Huang <charliehuang09@gmail.com>
    
    * wip
    
    Signed-off-by: Charlie Huang <charliehuang09@gmail.com>
    
    * tested on orin
    
    Signed-off-by: Charlie Huang <charliehuang09@gmail.com>
    
    * benchmark
    
    results are dissapointing
    
    Signed-off-by: Charlie Huang <charliehuang09@gmail.com>
    
    * wip
    
    Signed-off-by: Charlie Huang <charliehuang09@gmail.com>
    
    * small cleanup
    
    Signed-off-by: Charlie Huang <charliehuang09@gmail.com>
    
    * adress comments
    
    Signed-off-by: Charlie Huang <charliehuang09@gmail.com>
    
    ---------
    
    Signed-off-by: Charlie Huang <charliehuang09@gmail.com>
    Former-commit-id: 5f6484a3be206556311b5ac0b5eff6ce0b908b7c

---
+++ src/localization/nvidia_apriltag_detector.cc
@@ -0,0 +1,114 @@
+#include "src/localization/nvidia_apriltag_detector.h"
+#include <fmt/chrono.h>
+#include <vpi/Array.h>
+#include <vpi/Stream.h>
+#include <Eigen/Geometry>
+#include <cmath>
+#include <opencv2/imgproc.hpp>
+#include <vpi/OpenCVInterop.hpp>
+#include "src/localization/position.h"
+#include "src/utils/log.h"
+#include "src/utils/timer.h"
+
+namespace localization {
+
+frc::Pose3d Transform3dFromMatrix(float matrix[3][4]) {
+  // Need to convert to wpilib coordinates
+  const float x_translation = matrix[2][3];
+  const float y_translation = matrix[0][3];
+  const float z_translation = matrix[1][3];
+
+  Eigen::Matrix3d rotation_matrix{{matrix[2][2], matrix[2][0], matrix[2][1]},
+                                  {matrix[0][2], matrix[0][0], matrix[0][1]},
+                                  {matrix[1][2], matrix[1][0], matrix[1][1]}};
+
+  // Converting to quaternion because wpilib fails to directly convert from matrix
+  Eigen::Quaterniond quaternion(rotation_matrix);
+  return frc::Pose3d(
+      frc::Translation3d(units::meter_t{x_translation},
+                         units::meter_t{y_translation},
+                         units::meter_t{z_translation}),
+      frc::Rotation3d(frc::Quaternion(quaternion.w(), quaternion.x(),
+                                      quaternion.y(), quaternion.z())));
+}
+
+NvidiaAprilTagDetector::NvidiaAprilTagDetector(
+    int image_width, int image_height, nlohmann::json intrinsics,
+    VPIAprilTagDecodeParams params, VPIBackend backend, int max_detections,
+    std::vector<cv::Point3f> apriltag_dimensions, bool verbose)
+    : params_(params),
+      backend_(backend),
+      max_detections_(max_detections),
+      apriltag_dimensions_(apriltag_dimensions),
+      input_(nullptr) {
+
+  intrinsics_[0][0] = intrinsics["fx"];
+  intrinsics_[0][2] = intrinsics["cx"];
+  intrinsics_[1][1] = intrinsics["fy"];
+  intrinsics_[1][2] = intrinsics["cy"];
+
+  (vpiCreateAprilTagDetector(backend_, image_width, image_height, &params_,
+                             &payload_));
+
+  (vpiArrayCreate(max_detections_, VPI_ARRAY_TYPE_APRILTAG_DETECTION, 0,
+                  &detections_));
+  (vpiArrayCreate(max_detections_, VPI_ARRAY_TYPE_POSE, 0, &poses_));
+  (vpiStreamCreate(0, &stream_));
+}
+
+std::vector<tag_detection_t> NvidiaAprilTagDetector::GetTagDetections(
+    camera::timestamped_frame_t& timestamped_frame) {
+  cv::Mat gray;
+
+  if (timestamped_frame.frame.channels() == 1) {
+    gray = timestamped_frame.frame;
+  } else if (timestamped_frame.frame.channels() == 3) {
+    cv::cvtColor(timestamped_frame.frame, gray, cv::COLOR_BGR2GRAY);
+  }
+
+  if (input_ == nullptr) {
+    (vpiImageCreateWrapperOpenCVMat(gray, 0, &input_));
+  } else {
+    (vpiImageSetWrappedOpenCVMat(input_, gray));
+  }
+
+  (vpiSubmitAprilTagDetector(stream_, backend_, payload_, max_detections_,
+                             input_, detections_));
+
+  (vpiSubmitAprilTagPoseEstimation(stream_, VPI_BACKEND_CPU, detections_,
+                                   intrinsics_, ktag_size, poses_));
+
+  vpiStreamSync(stream_);
+
+  VPIArrayData detections_data{};
+  vpiArrayLockData(detections_, VPI_LOCK_READ, VPI_ARRAY_BUFFER_HOST_AOS,
+                   &detections_data);
+
+  VPIArrayData poses_data{};
+  vpiArrayLockData(poses_, VPI_LOCK_READ, VPI_ARRAY_BUFFER_HOST_AOS,
+                   &poses_data);
+
+  VPIPose* p = static_cast<VPIPose*>(poses_data.buffer.aos.data);
+  if (*detections_data.buffer.aos.sizePointer != 0) {
+    frc::Pose3d camera_relitive_position = Transform3dFromMatrix(p->transform);
+    PrintPose3d(camera_relitive_position);
+    vpiArrayUnlock(detections_);
+    vpiArrayUnlock(poses_);
+    return std::vector<tag_detection_t>(
+        {tag_detection_t{camera_relitive_position, timestamped_frame.timestamp,
+                         std::hypot(camera_relitive_position.X().value(),
+                                    camera_relitive_position.Y().value())}});
+  }
+
+  vpiArrayUnlock(detections_);
+  vpiArrayUnlock(poses_);
+  return std::vector<tag_detection_t>();
+}
+
+NvidiaAprilTagDetector::~NvidiaAprilTagDetector() {
+  vpiStreamDestroy(stream_);
+  vpiArrayDestroy(detections_);
+  vpiArrayDestroy(poses_);
+  vpiPayloadDestroy(payload_);
+}
+}  // namespace localization
