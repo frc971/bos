f6ce1266249d96f86f000a923723f6e3b07d96f6
Charlie Huang <charliehuang09@gmail.com>
2026-01-06 21:56:38 +0000

    add camera constants for software bot
    Signed-off-by: Charlie Huang <charliehuang09@gmail.com>

--- src/software_bot_main.cc
+++ src/software_bot_main.cc
@@ -27,23 +27,25 @@ auto main() -> int {
   utils::StartNetworktables();
 
   camera::CameraSource back_left_camera = camera::CameraSource(
-      "back_left",
-      std::make_unique<camera::CVCamera>(cv::VideoCapture(
-          camera::camera_constants[camera::Camera::FIDDLER_USB0].pipeline)));
+      "turret", std::make_unique<camera::CVCamera>(cv::VideoCapture(
+                    camera::camera_constants[camera::Camera::SOFTWARE_BOT_USB0]
+                        .pipeline)));
 
   camera::CameraSource back_right_camera = camera::CameraSource(
-      "back_right",
+      "ground_intake",
       std::make_unique<camera::CVCamera>(cv::VideoCapture(
-          camera::camera_constants[camera::Camera::FIDDLER_USB1].pipeline)));
+          camera::camera_constants[camera::Camera::SOFTWARE_BOT_USB1]
+              .pipeline)));
 
   std::thread usb0_thread(
       localization::run_localization, std::ref(back_left_camera),
       std::make_unique<localization::GPUAprilTagDetector>(
           640, 480,
           utils::read_intrinsics(
-              camera::camera_constants[camera::Camera::FIDDLER_USB0]
+              camera::camera_constants[camera::Camera::SOFTWARE_BOT_USB0]
                   .intrinsics_path)),
-      camera::camera_constants[camera::Camera::FIDDLER_USB0].extrinsics_path,
+      camera::camera_constants[camera::Camera::SOFTWARE_BOT_USB0]
+          .extrinsics_path,
       4971, false);
 
   std::thread usb1_thread(
@@ -51,9 +53,10 @@ auto main() -> int {
       std::make_unique<localization::GPUAprilTagDetector>(
           1280, 720,
           utils::read_intrinsics(
-              camera::camera_constants[camera::Camera::FIDDLER_USB1]
+              camera::camera_constants[camera::Camera::SOFTWARE_BOT_USB1]
                   .intrinsics_path)),
-      camera::camera_constants[camera::Camera::FIDDLER_USB1].extrinsics_path,
+      camera::camera_constants[camera::Camera::SOFTWARE_BOT_USB1]
+          .extrinsics_path,
       4972, false);
 
   nt::NetworkTableInstance inst = nt::NetworkTableInstance::GetDefault();
@@ -74,10 +77,10 @@ auto main() -> int {
       std::ref(model_info.class_names), std::ref(back_left_camera),
       std::ref(coral_topic), std::ref(algae_topic),
       utils::read_intrinsics(
-          camera::camera_constants[camera::Camera::FIDDLER_USB1]
+          camera::camera_constants[camera::Camera::SOFTWARE_BOT_USB1]
               .intrinsics_path),
       utils::read_extrinsics(
-          camera::camera_constants[camera::Camera::FIDDLER_USB1]
+          camera::camera_constants[camera::Camera::SOFTWARE_BOT_USB1]
               .extrinsics_path),
       true);
 
