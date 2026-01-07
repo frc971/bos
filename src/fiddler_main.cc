2fce006e30982ef67062fbcb1e82a44f70578311
Nano <nanoticity@gmail.com>
2026-01-07 18:53:26 +0000

    Only usb1

--- src/fiddler_main.cc
+++ src/fiddler_main.cc
@@ -23,25 +23,10 @@ using json = nlohmann::json;
 auto main() -> int {
   utils::StartNetworktables();
 
-  camera::CameraSource back_left_camera = camera::CameraSource(
-      "back_left",
-      std::make_unique<camera::CVCamera>(cv::VideoCapture(
-          camera::camera_constants[camera::Camera::FIDDLER_USB0].pipeline)));
-
   camera::CameraSource back_right_camera = camera::CameraSource(
       "back_right",
       std::make_unique<camera::CVCamera>(cv::VideoCapture(
           camera::camera_constants[camera::Camera::FIDDLER_USB1].pipeline)));
-
-  std::thread usb0_thread(
-      localization::run_localization, std::ref(back_left_camera),
-      std::make_unique<localization::GPUAprilTagDetector>(
-          640, 480,
-          utils::read_intrinsics(
-              camera::camera_constants[camera::Camera::FIDDLER_USB0]
-                  .intrinsics_path)),
-      camera::camera_constants[camera::Camera::FIDDLER_USB0].extrinsics_path,
-      4971, false);
 
   std::thread usb1_thread(
       localization::run_localization, std::ref(back_right_camera),
@@ -53,7 +38,7 @@ auto main() -> int {
       camera::camera_constants[camera::Camera::FIDDLER_USB1].extrinsics_path,
       4972, false);
 
-  usb0_thread.join();
+  usb1_thread.join();
 
   return 0;
 }
