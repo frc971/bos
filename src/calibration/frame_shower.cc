865f963a8c5e194807de43d481309bce329e7009
Charlie Huang <charliehuang09@gmail.com>
2026-01-14 15:17:12 -0800

    fix select_camera.cc
    Signed-off-by: Charlie Huang <charliehuang09@gmail.com>
    
    Former-commit-id: 643d18a14544029b70b443041e20abc5308016ac [formerly 643d18a14544029b70b443041e20abc5308016ac [formerly 643d18a14544029b70b443041e20abc5308016ac [formerly 5a4d919ffcde435405771baa4d8e4683dc58aebb]]]
    Former-commit-id: cba2827ac521488a3222b9cab54a01534db19dbe
    Former-commit-id: 906f8d134bf9d837a7653bb6e2c05ee7e9c60a52
    Former-commit-id: f78f0a847b8f4d57121b59b302cebd65a35bb9d4

--- src/calibration/frame_shower.cc
+++ src/calibration/frame_shower.cc
@@ -12,27 +12,16 @@
 #include "src/camera/select_camera.h"
 
 auto main() -> int {
-  camera::CscoreStreamer streamera("frame_shower", 4971, 30, 1080, 1080);
-  camera::CscoreStreamer streamerb("frame_shower", 4972, 30, 1080, 1080);
-  cv::VideoCapture a(0);
-  cv::VideoCapture b(1);
 
-  // std::unique_ptr<camera::ICamera> camera =
-  //     camera::GetCameraStream(camera::SelectCameraConfig());
-
-  cv::Mat framea;
-  cv::Mat frameb;
+  std::unique_ptr<camera::ICamera> camera =
+      camera::GetCameraStream(camera::SelectCameraConfig());
+  camera::CscoreStreamer streamer("frame_shower", 4971, 30, 1080, 1080);
 
   std::cout << "Camera opened successfully" << std::endl;
 
   while (true) {
-    std::cout << "Getting frame" << std::endl;
-    // a.read(framea);
-    // streamera.WriteFrame(framea);
-    b.read(frameb);
-    streamerb.WriteFrame(frameb);
-    std::cout << framea.size << std::endl;
-    std::cout << frameb.size << std::endl;
+    cv::Mat frame = camera->GetFrame().frame;
+    streamer.WriteFrame(frame);
   }
   cv::destroyAllWindows();
   return 0;
