dc2e191a49356b0868926bc850aa5924d021b0ff
yasen5 <yasengiuli@gmail.com>
2026-01-19 00:40:05 +0000

    Fix camera port and absl cmake
    Former-commit-id: 1ff4bd50fa127229b89b226f6b4caec7fb9acc1f [formerly 1ff4bd50fa127229b89b226f6b4caec7fb9acc1f [formerly 1b0ff4e180913caa1c6ccf767b1c91a70ae9342a]]
    Former-commit-id: b87e70cb1ff76b7aa9a255ca21ab26f7504216f9
    Former-commit-id: cf0c9cec9753dcc09bbd9c3c1d2792fa7182b1fe

--- src/turret_bot_main.cc
+++ src/turret_bot_main.cc
@@ -26,33 +26,33 @@ auto main() -> int {
   //     std::make_unique<camera::CVCamera>(cv::VideoCapture(
   //         camera::camera_constants[camera::Camera::IMX296_0].pipeline)));
   //
-  camera::CameraSource back_left_camera = camera::CameraSource(
-      "back_left",
-      std::make_unique<camera::CVCamera>(cv::VideoCapture(
-          camera::camera_constants[camera::Camera::IMX296_1].pipeline)));
+  // camera::CameraSource back_left_camera = camera::CameraSource(
+  //     "back_left",
+  //     std::make_unique<camera::CVCamera>(cv::VideoCapture(
+  //         camera::camera_constants[camera::Camera::IMX296_1].pipeline)));
 
-  // std::thread front_right_thread(
-  //     localization::run_localization, std::ref(front_right_camera),
-  //     std::make_unique<localization::GPUAprilTagDetector>(
-  //         front_right_camera.GetFrame().cols,
-  //         front_right_camera.GetFrame().rows,
-  //         utils::read_intrinsics(
-  //             camera::camera_constants[camera::Camera::TURRET_BOT_FRONT_RIGHT]
-  //                 .intrinsics_path)),
-  //     camera::camera_constants[camera::Camera::TURRET_BOT_FRONT_RIGHT]
-  //         .extrinsics_path,
-  //     4971, false);
-  //
-  // std::thread front_left_thread(
-  //     localization::run_localization, std::ref(front_left_camera),
-  //     std::make_unique<localization::GPUAprilTagDetector>(
-  //         front_left_camera.GetFrame().cols, front_left_camera.GetFrame().rows,
-  //         utils::read_intrinsics(
-  //             camera::camera_constants[camera::Camera::TURRET_BOT_FRONT_LEFT]
-  //                 .intrinsics_path)),
-  //     camera::camera_constants[camera::Camera::TURRET_BOT_FRONT_LEFT]
-  //         .extrinsics_path,
-  //     4972, false);
+  std::thread front_right_thread(
+      localization::run_localization, std::ref(front_right_camera),
+      std::make_unique<localization::GPUAprilTagDetector>(
+          front_right_camera.GetFrame().cols,
+          front_right_camera.GetFrame().rows,
+          utils::read_intrinsics(
+              camera::camera_constants[camera::Camera::TURRET_BOT_FRONT_RIGHT]
+                  .intrinsics_path)),
+      camera::camera_constants[camera::Camera::TURRET_BOT_FRONT_RIGHT]
+          .extrinsics_path,
+      4971, false);
+  
+  std::thread front_left_thread(
+      localization::run_localization, std::ref(front_left_camera),
+      std::make_unique<localization::GPUAprilTagDetector>(
+          front_left_camera.GetFrame().cols, front_left_camera.GetFrame().rows,
+          utils::read_intrinsics(
+              camera::camera_constants[camera::Camera::TURRET_BOT_FRONT_LEFT]
+                  .intrinsics_path)),
+      camera::camera_constants[camera::Camera::TURRET_BOT_FRONT_LEFT]
+          .extrinsics_path,
+      4972, false);
 
   // std::thread back_right_thread(
   //     localization::run_localization, std::ref(back_right_camera),
@@ -65,16 +65,16 @@ auto main() -> int {
   //         .extrinsics_path,
   //     4973, false);
   //
-  std::thread back_left_thread(
-      localization::run_localization, std::ref(back_left_camera),
-      std::make_unique<localization::GPUAprilTagDetector>(
-          back_left_camera.GetFrame().cols, back_left_camera.GetFrame().rows,
-          utils::read_intrinsics(
-              camera::camera_constants[camera::Camera::TURRET_BOT_BACK_LEFT]
-                  .intrinsics_path)),
-      camera::camera_constants[camera::Camera::TURRET_BOT_BACK_LEFT]
-          .extrinsics_path,
-      4974, false);
+  // std::thread back_left_thread(
+  //    localization::run_localization, std::ref(back_left_camera),
+  //    std::make_unique<localization::GPUAprilTagDetector>(
+  //        back_left_camera.GetFrame().cols, back_left_camera.GetFrame().rows,
+  //        utils::read_intrinsics(
+  //            camera::camera_constants[camera::Camera::TURRET_BOT_BACK_LEFT]
+  //                .intrinsics_path)),
+  //    camera::camera_constants[camera::Camera::TURRET_BOT_BACK_LEFT]
+  //        .extrinsics_path,
+  //    4974, false);
 
-  back_left_thread.join();
+  front_left_thread.join();
 }
