2ba967f468eb0e9a1d2210f819699b1969d9005a
Charlie Huang <charliehuang09@gmail.com>
2026-01-20 00:03:05 +0000

    change extrinsics format
    Signed-off-by: Charlie Huang <charliehuang09@gmail.com>
    
    Former-commit-id: c5095ac11323516288ae330f667d578885b7574e [formerly c5095ac11323516288ae330f667d578885b7574e [formerly c5095ac11323516288ae330f667d578885b7574e [formerly c94e78fb62e4ad9063c28872a33c7ff78f0d372b]]]
    Former-commit-id: c4f676d705b9b307054bad9901b5f4ca1a5822df
    Former-commit-id: dda64bfc9ad996b56e5dc4c7cc76b874fc1140a2
    Former-commit-id: a5f2776be2a41e69c9aad8f8fa8b682823247492

--- src/localization/get_field_relitive_position.cc
+++ src/localization/get_field_relitive_position.cc
@@ -64,13 +64,14 @@ auto ToFeildRelitivePosition(tag_detection_t tag_relative_position,
 
 auto ExtrinsicsJsonToCameraToRobot(nlohmann::json extrinsics_json)
     -> frc::Transform3d {
-  frc::Transform3d robot_to_camera(
+  frc::Pose3d camera_pose(
       units::meter_t{extrinsics_json["translation_x"]},
       units::meter_t{extrinsics_json["translation_y"]},
       units::meter_t{extrinsics_json["translation_z"]},
       frc::Rotation3d(units::radian_t{extrinsics_json["rotation_x"]},
                       units::radian_t{extrinsics_json["rotation_y"]},
                       units::radian_t{extrinsics_json["rotation_z"]}));
+  frc::Transform3d robot_to_camera(frc::Pose3d(), camera_pose);
   return robot_to_camera.Inverse();
 }
 
