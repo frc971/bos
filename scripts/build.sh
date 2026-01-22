6c885a2b3ee6fe4a227384b2f9083c2f9dbd8ae6
Charlie Huang <charliehuang09@gmail.com>
2026-01-22 16:26:14 +0000

    default build builds with clang-tidy
    Signed-off-by: Charlie Huang <charliehuang09@gmail.com>
    
    Former-commit-id: 8ca0a6270ac49194e5e3b622c24eba2d253a52c1 [formerly 8ca0a6270ac49194e5e3b622c24eba2d253a52c1 [formerly e898b12536e3ef9d65ed05d116825991f841cdf8]]
    Former-commit-id: cbae55c7564efee7a763ce34263f0f133db255ca
    Former-commit-id: 6cae0bd3ef2cb8e775f4df9b0c8f45f549624b45

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,1 +1,1 @@
-git submodule init && git submodule update && cmake -B build -G Ninja . && cmake --build build
+git submodule init && git submodule update && cmake -DENABLE_CLANG_TIDY=OFF -B build -G Ninja . && cmake --build build
