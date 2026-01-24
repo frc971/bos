e470123c2a040571952d5ccfc500699252c819ea
Charlie Huang <charliehuang09@gmail.com>
2026-01-24 03:09:35 +0000

    use joint solve
    Signed-off-by: Charlie Huang <charliehuang09@gmail.com>

--- src/turret_bot_main.cc
+++ src/turret_bot_main.cc
@@ -40,7 +40,7 @@ auto main() -> int {
           utils::read_intrinsics(
               camera::camera_constants[camera::Camera::TURRET_BOT_FRONT_RIGHT]
                   .intrinsics_path)),
-      std::make_unique<localization::SquareSolver>(
+      std::make_unique<localization::JointSolver>(
           camera::Camera::TURRET_BOT_FRONT_RIGHT),
       camera::camera_constants[camera::Camera::TURRET_BOT_FRONT_RIGHT]
           .extrinsics_path,
