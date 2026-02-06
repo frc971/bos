4fdd1d23c3f2e262d94594cb40f09e7b38637648
charliehuang09 <41605449+charliehuang09@users.noreply.github.com>
2026-02-05 18:46:52 -0800

    add solver test (#86)
    * add solver test
    
    Signed-off-by: Charlie Huang <charliehuang09@gmail.com>
    
    * Make test more scalable
    
    * Remove unnecessary debug statements
    
    ---------
    
    Signed-off-by: Charlie Huang <charliehuang09@gmail.com>
    Co-authored-by: yasen5 <carrotmercinary@gmail.com>

---
+++ src/test/solver_test.cc
@@ -0,0 +1,32 @@
+#include "src/localization/square_solver.h"
+#include "src/utils/camera_utils.h"
+#include "src/utils/intrinsics_from_json.h"
+constexpr int kimage_tag_width = 20;
+constexpr int kimage_tag_height = 20;
+constexpr std::array<int, 2> ktag_ids = {15, 31};
+constexpr int kimage_width = 20;
+constexpr int kimage_height = 20;
+
+auto main() -> int {
+  const std::array<cv::Point2f, 4> image_points = {
+      cv::Point2f(kimage_width / 2.0 - kimage_tag_width / 2.0,
+                  kimage_height / 2.0 + kimage_tag_height / 2.0),
+      cv::Point2f(kimage_width / 2.0 + kimage_tag_width / 2.0,
+                  kimage_height / 2.0 + kimage_tag_height / 2.0),
+      cv::Point2f(kimage_width / 2.0 + kimage_tag_width / 2.0,
+                  kimage_height / 2.0 - kimage_tag_height / 2.0),
+      cv::Point2f(kimage_width / 2.0 - kimage_tag_width / 2.0,
+                  kimage_height / 2.0 - kimage_tag_height / 2.0)};
+  localization::SquareSolver solver(camera::Camera::DUMMY_CAMERA);
+
+  for (const int id : ktag_ids) {
+    const localization::tag_detection_t fake_detection{.tag_id = id,
+                                                       .corners = image_points};
+    const std::vector<localization::tag_detection_t> fake_detections{
+        fake_detection};
+    std::cout << id << ":\n"
+              << solver.EstimatePosition(fake_detections)[0] << std::endl;
+  }
+
+  return 0;
+}
