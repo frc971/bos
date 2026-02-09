a14d9b58296728e9e7661978ee4d2a578dc0e04d
yasen5 <carrotmercinary@gmail.com>
2026-02-09 00:05:57 +0000

    Add gtest and debug setup
    Former-commit-id: b58d8fcb5bac1f4fa349e12cc3ae091abe295ba2 [formerly b58d8fcb5bac1f4fa349e12cc3ae091abe295ba2 [formerly b58d8fcb5bac1f4fa349e12cc3ae091abe295ba2 [formerly 97f2d34740eee992269b23ac7e08d4b1337bde38]]]
    Former-commit-id: 6a1db991f91cd7c39df438a35e98d506ec7ac6f8
    Former-commit-id: cdae2c0ae64eb673fd0e1d695fde4aaaa54a86f8
    Former-commit-id: 1a3e7d5e7aef791a5051564ea0e06521e4dff9e5

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,14 @@
 git submodule init 
 git submodule update 
-cmake -DENABLE_CLANG_TIDY=OFF -DABSL_BUILD_TESTING=OFF -B build -G Ninja . 
+cmake -S . -B build -G Ninja \
+  -DCMAKE_BUILD_TYPE=Debug \
+  -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
+  -DENABLE_CLANG_TIDY=OFF \
+  -DABSL_BUILD_TESTING=OFF \
+  -DCMAKE_CXX_FLAGS_DEBUG="-O0 -g -fno-inline" \
+  -DCMAKE_C_FLAGS_DEBUG="-O0 -g -fno-inline"
 cmake --build build
 mkdir -p /bos 
-cp -r constants /bos
+if [ "$(pwd -P)/constants" != "/bos/constants" ]; then
+  cp -r constants /bos
+fi
--- src/test/solver_test.cc
+++ src/test/solver_test.cc
@@ -1,8 +1,12 @@
 #include <gtest/gtest.h>
+#include "src/localization/position.h"
 #include "src/localization/position_solver.h"
 #include "src/localization/square_solver.h"
 #include "src/utils/camera_utils.h"
 #include "src/utils/intrinsics_from_json.h"
+
+constexpr double ERROR_MARGIN_TRANSLATION = 0.01;
+constexpr double ERROR_MARGIN_EULER_ANGLE = 0.01745;  // 1 deg
 
 namespace frc {
 void PrintTo(const frc::Pose3d& pose, std::ostream* os) {
@@ -15,6 +19,26 @@ void PrintTo(const frc::Pose3d& pose, std::ostream* os) {
       << "]"
       << "}";
 }
+
+auto wrap_compare(double angle_diff) -> double {
+  double diff = std::abs(angle_diff);
+  if (std::abs(diff - 2 * std::numbers::pi) < ERROR_MARGIN_EULER_ANGLE) {
+    diff = 0;
+  }
+  return diff;
+}
+
+inline auto operator==(const localization::position_estimate_t& lhs,
+                       const localization::position_estimate_t& rhs) -> bool {
+  if (lhs.pose.Translation().Distance(rhs.pose.Translation()).value() >
+      ERROR_MARGIN_TRANSLATION)
+    return false;
+  const frc::Rotation3d rot_diff = lhs.pose.Rotation() - rhs.pose.Rotation();
+  const double euler_diff = wrap_compare(rot_diff.X().value()) +
+                            wrap_compare(rot_diff.Y().value()) +
+                            wrap_compare(rot_diff.Z().value());
+  return euler_diff == 0;
+}
 }  // namespace frc
 
 constexpr int kimage_tag_width = 20;
@@ -22,17 +46,21 @@ constexpr int kimage_tag_height = 20;
 constexpr std::array<int, 2> ktag_ids = {15, 31};
 constexpr int kimage_width = 20;
 constexpr int kimage_height = 20;
+const cv::Point2f tag_center =
+    cv::Point2f(kimage_width / 2.0, kimage_height / 2.0);
+const int dx = kimage_tag_width / 2.0;
+const int dy = kimage_tag_height / 2.0;
+// bottom left, bottom right, top right, top left
 const std::array<cv::Point2f, 4> image_points = {
-    cv::Point2f(kimage_width / 2.0 - kimage_tag_width / 2.0,
-                kimage_height / 2.0 + kimage_tag_height / 2.0),
-    cv::Point2f(kimage_width / 2.0 + kimage_tag_width / 2.0,
-                kimage_height / 2.0 + kimage_tag_height / 2.0),
-    cv::Point2f(kimage_width / 2.0 + kimage_tag_width / 2.0,
-                kimage_height / 2.0 - kimage_tag_height / 2.0),
-    cv::Point2f(kimage_width / 2.0 - kimage_tag_width / 2.0,
-                kimage_height / 2.0 - kimage_tag_height / 2.0)};
+    cv::Point2f(tag_center.x - dx, tag_center.y + dy),
+    cv::Point2f(tag_center.x + dx, tag_center.y + dy),
+    cv::Point2f(tag_center.x + dx, tag_center.y - dy),
+    cv::Point2f(tag_center.x - dx, tag_center.y - dy)};
 
 TEST(SolverTest, Basic) {
+  for (const auto& point : image_points) {
+    std::cout << point << std::endl;
+  }
   localization::SquareSolver solver(camera::Camera::DUMMY_CAMERA);
 
   for (const int id : ktag_ids) {
@@ -43,14 +71,15 @@ TEST(SolverTest, Basic) {
 
     localization::position_estimate_t estimate =
         solver.EstimatePosition(fake_detections)[0];
-    std::cout << ::testing::PrintToString(estimate.pose) << std::endl;
     frc::Pose3d flipped_tag =
         localization::kapriltag_layout.GetTagPose(id).value();
     flipped_tag = frc::Pose3d(
         flipped_tag.Translation(),
         frc::Rotation3d(
             flipped_tag.Rotation().X(), flipped_tag.Rotation().Y(),
-            flipped_tag.Rotation().Z() + units::radian_t{-std::numbers::pi}));
-    EXPECT_EQ(estimate.pose, flipped_tag);
+            flipped_tag.Rotation().Z() + units::radian_t{-std::numbers::pi / 2.0}));
+    localization::position_estimate_t expected{flipped_tag, 0, 0};
+    operator==(estimate, expected);
+    EXPECT_EQ(estimate, expected);
   }
 }
