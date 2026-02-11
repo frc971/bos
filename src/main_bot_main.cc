1fd745885527ba9c55168603af4fa18a65a70912
Charlie Huang <charliehuang09@gmail.com>
2026-02-11 20:19:17 +0000

    dp
    Signed-off-by: Charlie Huang <charliehuang09@gmail.com>
    
    Former-commit-id: 265b4fb7c8821f8c214fe468ef00946e07bc401c [formerly 265b4fb7c8821f8c214fe468ef00946e07bc401c [formerly 265b4fb7c8821f8c214fe468ef00946e07bc401c [formerly eae4654cdf91ac6962646ac128e9d193cadd1da9]]]
    Former-commit-id: 82c5f6b610abc565a9ce7b18a181077c8277a428
    Former-commit-id: fa531dcb3f295924fc4bac75b086701fb513be52
    Former-commit-id: 8f26037604f957544747020990766610df01dfcb

--- src/main_bot_main.cc
+++ src/main_bot_main.cc
@@ -5,15 +5,17 @@
 #include "src/localization/square_solver.h"
 #include "src/utils/camera_utils.h"
 #include "src/utils/nt_utils.h"
+
+using camera::Camera;
 auto main() -> int {
   utils::StartNetworktables();
   // TODO configure vision bot camera paths
 
   LOG(INFO) << "Starting cameras";
-  camera::CameraSource front_camera = camera::CameraSource(
-      "front",
-      std::make_unique<camera::CVCamera>(
-          camera::camera_constants[camera::Camera::MAIN_ROBOT_FRONT_CAMERA]));
+  // camera::CameraSource front_camera = camera::CameraSource(
+  //     "front",
+  //     std::make_unique<camera::CVCamera>(
+  //         camera::camera_constants[camera::Camera::MAIN_ROBOT_FRONT_CAMERA]));
 
   camera::CameraSource left_camera = camera::CameraSource(
       "front_left",
@@ -26,30 +28,31 @@ auto main() -> int {
           camera::camera_constants[camera::Camera::MAIN_ROBOT_RIGHT_CAMERA]));
 
   LOG(INFO) << "Started cameras";
+  std::this_thread::sleep_for(std::chrono::seconds(2));
   LOG(INFO) << "Starting estimators";
 
-  std::thread front_thread(
-      localization::run_localization, std::ref(front_camera),
-      std::make_unique<localization::GPUAprilTagDetector>(
-          front_camera.GetFrame().cols, front_camera.GetFrame().rows,
-          utils::read_intrinsics(
-              camera::camera_constants[camera::Camera::MAIN_ROBOT_FRONT_CAMERA]
-                  .intrinsics_path)),
-      std::make_unique<localization::SquareSolver>(
-          camera::Camera::MAIN_ROBOT_LEFT_CAMERA),
-      camera::camera_constants[camera::Camera::MAIN_ROBOT_FRONT_CAMERA]
-          .extrinsics_path,
-      4971, false);
+  // std::thread front_thread(
+  //     localization::run_localization, std::ref(front_camera),
+  //     std::make_unique<localization::GPUAprilTagDetector>(
+  //         front_camera.GetFrame().cols, front_camera.GetFrame().rows,
+  //         utils::read_intrinsics(
+  //             camera::camera_constants[camera::Camera::MAIN_ROBOT_FRONT_CAMERA]
+  //                 .intrinsics_path)),
+  //     std::make_unique<localization::SquareSolver>(
+  //         camera::Camera::MAIN_ROBOT_LEFT_CAMERA),
+  //     camera::camera_constants[camera::Camera::MAIN_ROBOT_FRONT_CAMERA]
+  //         .extrinsics_path,
+  //     4971, false);
 
   std::thread left_thread(
       localization::run_localization, std::ref(left_camera),
       std::make_unique<localization::GPUAprilTagDetector>(
           left_camera.GetFrame().cols, left_camera.GetFrame().rows,
           utils::read_intrinsics(
-              camera::camera_constants[camera::Camera::MAIN_ROBOT_LEFT_CAMERA]
+              camera::camera_constants[Camera::MAIN_ROBOT_LEFT_CAMERA]
                   .intrinsics_path)),
       std::make_unique<localization::SquareSolver>(
-          camera::Camera::MAIN_ROBOT_LEFT_CAMERA),
+          Camera::MAIN_ROBOT_LEFT_CAMERA),
       camera::camera_constants[camera::Camera::MAIN_ROBOT_LEFT_CAMERA]
           .extrinsics_path,
       4972, false);
@@ -59,15 +62,14 @@ auto main() -> int {
       std::make_unique<localization::GPUAprilTagDetector>(
           right_camera.GetFrame().cols, right_camera.GetFrame().rows,
           utils::read_intrinsics(
-              camera::camera_constants[camera::Camera::MAIN_ROBOT_RIGHT_CAMERA]
+              camera::camera_constants[Camera::MAIN_ROBOT_RIGHT_CAMERA]
                   .intrinsics_path)),
       std::make_unique<localization::SquareSolver>(
           camera::Camera::MAIN_ROBOT_RIGHT_CAMERA),
-      camera::camera_constants[camera::Camera::MAIN_ROBOT_RIGHT_CAMERA]
-          .extrinsics_path,
+      camera::camera_constants[Camera::MAIN_ROBOT_RIGHT_CAMERA].extrinsics_path,
       4973, false);
 
   LOG(INFO) << "Started estimators";
 
-  cv::waitKey(0);
+  right_thread.join();
 }
