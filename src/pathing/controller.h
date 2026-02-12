9a7c6af857f4e769ff24185594a38480467bda35
Nano <nanoticity@gmail.com>
2026-02-12 01:17:43 +0000

    Make the timestamping system cleaner, integrate with java code
    Signed-off-by: Nano <nanoticity@gmail.com>

---
+++ src/pathing/controller.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <frc/geometry/Pose2d.h>
+#include <networktables/NetworkTableInstance.h>
+#include <networktables/StructTopic.h>
+#include <cstdint>
+#include <atomic>
+
+namespace pathing {
+class Controller {
+ public:
+  Controller();
+  void Send();
+  void Stop();
+
+ private:
+  nt::NetworkTableInstance instance_;
+  nt::StructSubscriber<frc::Pose2d> current_pose_sub_;
+  nt::StructSubscriber<frc::Pose2d> target_pose_sub_;
+
+  int64_t kDtUs = 20'000;
+  double ax;
+  double ay;
+
+  std::atomic<bool> running_{false};
+};
+
+}  // namespace pathing
---
+++ src/pathing/velocity_sender.cc
@@ -0,0 +1,32 @@
+#include "src/pathing/velocity_sender.h"
+#include <networktables/DoubleTopic.h>
+#include <networktables/StructTopic.h>
+#include <units/angle.h>
+#include "frc/DataLogManager.h"
+#include <frc/Timer.h>
+
+namespace pathing {
+
+constexpr auto RadianToDegree(double radian) -> double {
+  return radian * (180 / M_PI);
+}
+
+VelocitySender::VelocitySender()
+    : instance_(nt::NetworkTableInstance::GetDefault()) {
+  std::shared_ptr<nt::NetworkTable> table =
+      instance_.GetTable("Orin/OTFVelocity/");
+
+  nt::DoubleArrayTopic vel_topic = table->GetDoubleArrayTopic("Velocity");
+  vel_publisher_ = vel_topic.Publish();
+}
+
+void VelocitySender::Send(const double ax, const double ay) {
+  if (mutex_.try_lock()) {
+    double timestamp = frc::Timer::GetFPGATimestamp().to<double>() +
+                       instance_.GetServerTimeOffset().value_or(0) / 1000000.0;
+    std::vector<double> vel_array = {ax, ay, timestamp};
+    vel_publisher_.Set(vel_array);
+    mutex_.unlock();
+  }
+}
+}  // namespace pathing
