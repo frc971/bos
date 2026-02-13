e2ed9423cb189cafd46c9ac7a296fe42473bd12b
Charlie Huang <charliehuang09@gmail.com>
2026-02-13 01:28:24 +0000

    refactor extrinsics
    Signed-off-by: Charlie Huang <charliehuang09@gmail.com>

--- src/utils/constants_from_json.h
+++ src/utils/constants_from_json.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <frc/geometry/Transform3d.h>
 #include "src/utils/pch.h"
 
 namespace utils {
@@ -9,4 +10,8 @@ auto camera_matrix_from_json(nlohmann::json intrinsics) -> T;
 
 template <typename T>
 auto distortion_coefficients_from_json(nlohmann::json intrinsics) -> T;
+
+auto ExtrinsicsJsonToCameraToRobot(nlohmann::json extrinsics_json)
+    -> frc::Transform3d;
+
 }  // namespace utils
