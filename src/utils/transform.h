4834c239eeb4a4e7b52eeede51637f23c66d9cbd
Charlie Huang <charliehuang09@gmail.com>
2026-02-13 21:12:29 +0000

    wip
    Signed-off-by: Charlie Huang <charliehuang09@gmail.com>
    
    Former-commit-id: 55102f5d96810db66327e971749ae9c500cbac2c [formerly 55102f5d96810db66327e971749ae9c500cbac2c [formerly 69bcd72b0ee9bda3cdc3799542389d5657f24bfb]]
    Former-commit-id: d03a437c641b80ca1c78dce41f98d6809c0a10ba
    Former-commit-id: 0f77eff2938a8507288d5bae3c302e89c74f1246

--- src/utils/transform.h
+++ src/utils/transform.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <frc/geometry/Pose3d.h>
 #include <opencv2/calib3d.hpp>
 #include "src/utils/pch.h"
 
@@ -13,4 +14,8 @@ auto CvMatToEigen(const cv::Mat& mat) -> Eigen::Matrix4d;
 
 auto ConvertOpencvCoordinateToWpilib(cv::Mat& vec) -> void;
 
+auto ConvertOpencvTransformationMatrixToWpilibPose(const cv::Mat& matrix)
+    -> frc::Pose3d;
+
+auto Pose3dToCvMat(frc::Pose3d) -> cv::Mat;
 }  // namespace utils
