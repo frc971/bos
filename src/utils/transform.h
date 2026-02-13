9553a34ae82508e347c990257ea9209e054a2b2b
Charlie Huang <charliehuang09@gmail.com>
2026-02-13 01:08:21 +0000

    refactor square solver utils functions
    questionable
    
    Signed-off-by: Charlie Huang <charliehuang09@gmail.com>
    
    Former-commit-id: 06ee46dd425d966631c5ed248fc12be4e512a55c [formerly 06ee46dd425d966631c5ed248fc12be4e512a55c [formerly d89d4578e3ce20f2c94ef999e875cc08359f3f41]]
    Former-commit-id: 4edfe11c375cb8f035d7faad6abb9ac904021ade
    Former-commit-id: 705a39abb90474a873df0a38a81a247d7a72671c

---
+++ src/utils/transform.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <opencv2/calib3d.hpp>
+#include "src/utils/pch.h"
+
+// Helper functions for converting between wpilib, opencv and eigen
+namespace utils {
+auto MakeTransform(const cv::Mat& rvec, const cv::Mat& tvec) -> cv::Mat;
+
+template <typename Derived>
+auto EigenToCvMat(const Eigen::MatrixBase<Derived>& mat) -> cv::Mat;
+
+auto CvMatToEigen(const cv::Mat& mat) -> Eigen::Matrix4d;
+
+auto ConvertOpencvCoordinateToWpilib(cv::Mat& vec) -> void;
+
+}  // namespace utils
