6ad9fc738e4eea4d135e70ca91bb2f9cb1b3e7d5
Charlie Huang <charliehuang09@gmail.com>
2026-02-14 02:01:31 +0000

    Remove tag layout json
    Because we updated to 2026
    
    Signed-off-by: Charlie Huang <charliehuang09@gmail.com>
    
    Former-commit-id: 6511c5be328a7836acf018fd3cdd740ec6e587a2 [formerly 6511c5be328a7836acf018fd3cdd740ec6e587a2 [formerly c7ce64ddbcdddb8d7ef2d83a632fa712e71db4f1]]
    Former-commit-id: 2f344f1f41ecdd40fe7054d70341d76eb8c5e992
    Former-commit-id: ddc458f5194e9a8a895f6018ab019a2ceef50431

--- src/localization/position_solver.h
+++ src/localization/position_solver.h
@@ -15,7 +15,8 @@ const std::vector<cv::Point3f> kapriltag_corners = {
     {-ktag_size / 2, -ktag_size / 2, 0}};
 
 const frc::AprilTagFieldLayout kapriltag_layout =
-    frc::AprilTagFieldLayout("/bos/constants/2026-rebuilt-andymark.json");
+    frc::AprilTagFieldLayout::LoadField(
+        frc::AprilTagField::k2026RebuiltAndyMark);
 
 // Interface for a class when given a apriltag detections, uses the detections to get the position of the robot
 class IPositionSolver {
