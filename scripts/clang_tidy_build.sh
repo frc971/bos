413e4f7d34111ea88f6e4982c0217bf88eac4eae
Charlie Huang <charliehuang09@gmail.com>
2026-02-16 03:31:54 +0000

    fix clang tidy build
    Signed-off-by: Charlie Huang <charliehuang09@gmail.com>

--- scripts/clang_tidy_build.sh
+++ scripts/clang_tidy_build.sh
@@ -1,1 +1,5 @@
-git submodule init && git submodule update && cmake -DENABLE_CLANG_TIDY=ON -B build -G Ninja . && cmake --build build
+git submodule init 
+git submodule update 
+cmake -DENABLE_CLANG_TIDY=ON -B build -G Ninja . 
+cmake --build build
+cp -r constants /bos
