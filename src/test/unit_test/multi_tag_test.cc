b56a521e3e95797d00bcac8a2b7fcc7bbd67d5f8
Charlie Huang <charliehuang09@gmail.com>
2026-02-16 02:48:36 +0000

    multi tag solve unit test
    Signed-off-by: Charlie Huang <charliehuang09@gmail.com>

---
+++ src/test/unit_test/multi_tag_test.cc
@@ -0,0 +1,26 @@
+#include <gtest/gtest.h>
+#include "src/localization/multi_tag_solver.h"
+#include "src/localization/square_solver.h"
+
+using camera::Camera;
+
+TEST(HelloTest, BasicAssertions) {  // NOLINT
+  auto config = camera::Camera::DEV_ORIN;
+
+  std::vector<localization::tag_detection_t> detections(
+      {{.tag_id = 25,
+        .corners = {cv::Point2d(100.0f, 100.0f), cv::Point2d(200.0f, 100.0f),
+                    cv::Point2d(200.0f, 200.0f), cv::Point2d(100.0f, 200.0f)},
+        .timestamp = 0.0,
+        .confidence = 0.0}});
+
+  localization::SquareSolver square_solver(config);
+  localization::MultiTagSolver multi_tag_solver(config);
+
+  auto square_solver_solution = square_solver.EstimatePosition(detections)[0];
+  utils::PrintPose3d(square_solver_solution.pose);
+
+  auto multi_tag_solution = square_solver.EstimatePosition(detections)[0];
+  utils::PrintPose3d(multi_tag_solution.pose);
+
+}  // NOLINT
