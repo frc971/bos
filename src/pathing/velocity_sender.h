778a0fc159b7aca1537e5b3285875951744e13cd
Nano <nanoticity@gmail.com>
2026-02-28 00:09:31 +0000

    Add velocity_cender.h

---
+++ src/pathing/velocity_sender.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <frc/geometry/Pose2d.h>
+#include <networktables/BooleanTopic.h>
+#include <networktables/DoubleArrayTopic.h>
+#include <networktables/DoubleTopic.h>
+#include <networktables/NetworkTable.h>
+#include <networktables/NetworkTableInstance.h>
+#include <networktables/StructTopic.h>
+#include "src/utils/pch.h"
+
+namespace pathing {
+class VelocitySender {
+ public:
+  VelocitySender();
+  void Send(const double ax, const double ay);
+
+ private:
+  nt::NetworkTableInstance instance_;
+
+  nt::DoubleArrayPublisher vel_publisher_;
+
+  std::mutex mutex_;
+};
+}  // namespace pathing
