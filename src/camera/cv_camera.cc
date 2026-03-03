37912e8ca8bf31fdaf5f2af8821c15dfb3b1d016
Charlie Huang <charliehuang09@gmail.com>
2026-03-03 00:28:18 +0000

    change cv camera
    Signed-off-by: Charlie Huang <charliehuang09@gmail.com>

--- src/camera/cv_camera.cc
+++ src/camera/cv_camera.cc
@@ -17,7 +17,9 @@ auto FileSystemAlmostFull() {
 }
 
 CVCamera::CVCamera(const CameraConstant& c, std::optional<std::string> log_path)
-    : cap_(cv::VideoCapture(c.pipeline)), log_path_(std::move(log_path)) {
+    : cap_(cv::VideoCapture(c.pipeline)),
+      pipeline_(c.pipeline),
+      log_path_(std::move(log_path)) {
   if (FileSystemAlmostFull()) {
     log_path_ = std::nullopt;
     LOG(WARNING) << "Filesystem almost full! Not logging any frames";
@@ -75,8 +77,9 @@ auto CVCamera::GetFrame() -> timestamped_frame_t {
 
 auto CVCamera::Restart() -> void {
   cap_.release();
-  cap_ = cv::VideoCapture(pipeline_);
   std::this_thread::sleep_for(std::chrono::seconds(3));
+  LOG(INFO) << "Pipeline: " << pipeline_;
+  cap_ = cv::VideoCapture(pipeline_);
 }
 
 }  // namespace camera
