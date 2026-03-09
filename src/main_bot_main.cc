96e4983f9a3c3a82471fc45787346f42cae2b717
Charlie Huang <charliehuang09@gmail.com>
2026-03-09 01:30:10 +0000

    uncomment code
    Signed-off-by: Charlie Huang <charliehuang09@gmail.com>

--- src/main_bot_main.cc
+++ src/main_bot_main.cc
@@ -36,38 +36,38 @@ auto main() -> int {
   std::this_thread::sleep_for(std::chrono::seconds(2));
   LOG(INFO) << "Starting estimators";
 
-  // std::thread front_thread(
-  //     localization::RunLocalization, std::ref(front_camera),
-  //     std::make_unique<localization::GPUAprilTagDetector>(
-  //         front_camera.GetFrame().cols, front_camera.GetFrame().rows,
-  //         utils::ReadIntrinsics(
-  //             camera_constants.at("main_bot_front").intrinsics_path.value())),
-  //     std::make_unique<localization::MultiTagSolver>(
-  //         camera_constants.at("main_bot_front")),
-  //     camera_constants.at("main_bot_front").extrinsics_path, 4971, false);
-  //
-  // std::thread left_thread(
-  //     localization::RunLocalization, std::ref(left_camera),
-  //     std::make_unique<localization::OpenCVAprilTagDetector>(
-  //         left_camera.GetFrame().cols, left_camera.GetFrame().rows,
-  //         utils::ReadIntrinsics(
-  //             camera_constants.at("main_bot_left").intrinsics_path.value())),
-  //     std::make_unique<localization::MultiTagSolver>(
-  //         camera_constants.at("main_bot_left")),
-  //     camera_constants.at("main_bot_left").extrinsics_path, 4972, false);
-  //
-  // std::thread right_thread(
-  //     localization::RunLocalization, std::ref(right_camera),
-  //     std::make_unique<localization::OpenCVAprilTagDetector>(
-  //         right_camera.GetFrame().cols, right_camera.GetFrame().rows,
-  //         utils::ReadIntrinsics(
-  //             camera_constants.at("main_bot_right").intrinsics_path.value())),
-  //     std::make_unique<localization::MultiTagSolver>(
-  //         camera_constants.at("main_bot_right")),
-  //     camera_constants.at("main_bot_right").extrinsics_path, 4973, false);
-  //
-  // LOG(INFO) << "Started estimators";
-  //
-  // // TODO find better way
-  // left_thread.join();
+  std::thread front_thread(
+      localization::RunLocalization, std::ref(front_camera),
+      std::make_unique<localization::GPUAprilTagDetector>(
+          front_camera.GetFrame().cols, front_camera.GetFrame().rows,
+          utils::ReadIntrinsics(
+              camera_constants.at("main_bot_front").intrinsics_path.value())),
+      std::make_unique<localization::MultiTagSolver>(
+          camera_constants.at("main_bot_front")),
+      camera_constants.at("main_bot_front").extrinsics_path, 4971, false);
+
+  std::thread left_thread(
+      localization::RunLocalization, std::ref(left_camera),
+      std::make_unique<localization::OpenCVAprilTagDetector>(
+          left_camera.GetFrame().cols, left_camera.GetFrame().rows,
+          utils::ReadIntrinsics(
+              camera_constants.at("main_bot_left").intrinsics_path.value())),
+      std::make_unique<localization::MultiTagSolver>(
+          camera_constants.at("main_bot_left")),
+      camera_constants.at("main_bot_left").extrinsics_path, 4972, false);
+
+  std::thread right_thread(
+      localization::RunLocalization, std::ref(right_camera),
+      std::make_unique<localization::OpenCVAprilTagDetector>(
+          right_camera.GetFrame().cols, right_camera.GetFrame().rows,
+          utils::ReadIntrinsics(
+              camera_constants.at("main_bot_right").intrinsics_path.value())),
+      std::make_unique<localization::MultiTagSolver>(
+          camera_constants.at("main_bot_right")),
+      camera_constants.at("main_bot_right").extrinsics_path, 4973, false);
+
+  LOG(INFO) << "Started estimators";
+
+  // TODO find better way
+  left_thread.join();
 }
