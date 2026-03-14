788e1c38ab1746e6b9c88285979bd4daee6951ee
yasen5 <167659798+yasen5@users.noreply.github.com>
2026-03-13 19:29:48 -0700

    Fix disk camera (#130)

--- src/camera/disk_camera.cc
+++ src/camera/disk_camera.cc
@@ -5,16 +5,15 @@ namespace camera {
 
 DiskCamera::DiskCamera(std::string image_folder_path)
     : image_folder_path_(std::move(image_folder_path)) {
-  for (auto& entry : std::filesystem::directory_iterator(image_folder_path)) {
-    std::cout << entry;
-    std::string folder_path = entry.path().string();
+  for (auto& entry : std::filesystem::directory_iterator(image_folder_path_)) {
+    std::string entry_name = entry.path().filename().string();
 
     // remove .png for timestamp
     image_paths_.push(timestamped_frame_path_t{
         .path = entry.path(),
-        .timestamp = std::stod(entry.path().filename().string().erase(
-            folder_path.size() - 4, 4))});
+        .timestamp = std::stod(entry_name.erase(entry_name.size() - 4, 4))});
   }
+  timer_.Reset();
 }
 auto DiskCamera::GetFrame() -> timestamped_frame_t {
   if (image_paths_.empty()) {
@@ -27,11 +26,15 @@ auto DiskCamera::GetFrame() -> timestamped_frame_t {
       .timestamp = image_paths_.top().timestamp};
   image_paths_.pop();
 
-  auto timestamp = frc::Timer::GetFPGATimestamp().to<double>();
+  auto timestamp = timer_.Get().value();
+  std::cout << "FPGA timestamp: " << timestamp << std::endl;
   while (timestamp < image_paths_.top().timestamp) {
+    std::cout << "Frame Timestamp: " << image_paths_.top().timestamp
+              << std::endl;
     std::this_thread::sleep_for(std::chrono::duration<double>(
         image_paths_.top().timestamp - timestamp));
-    timestamp = frc::Timer::GetFPGATimestamp().to<double>();
+    image_paths_.pop();
+    timestamp = timer_.Get().value();
   }
   return timestamped_frame;
 }
