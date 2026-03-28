954aff0f9e0115f4bced807588db3e3be282f4d3
yasen5 <167659798+yasen5@users.noreply.github.com>
2026-03-28 15:49:50 +0000

    Update to include release vs debug

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 set -e
 
-# Default name
+# Defaults
 NAME=""
+TYPE="Release"
 
 # Parse args
 for arg in "$@"; do
@@ -11,19 +12,32 @@ for arg in "$@"; do
       NAME="${arg#*=}"
       shift
       ;;
+    --type=*)
+      TYPE="${arg#*=}"
+      shift
+      ;;
   esac
 done
 
+# Normalize / validate type
+if [[ "$TYPE" != "Release" && "$TYPE" != "Debug" ]]; then
+  echo "Invalid --type: $TYPE (must be Release or Debug)"
+  exit 1
+fi
+
+# Lowercase version for directory naming
+TYPE_LOWER=$(echo "$TYPE" | tr '[:upper:]' '[:lower:]')
+
 # Determine build directory
 if [ -z "$NAME" ]; then
-  BUILD_DIR="build"
+  BUILD_DIR="${TYPE_LOWER}-build"
 else
-  BUILD_DIR="${NAME}-build"
+  BUILD_DIR="${NAME}-${TYPE_LOWER}-build"
 fi
 
 git submodule init
 git submodule update
-cmake -DENABLE_CLANG_TIDY=OFF -DCMAKE_BUILD_TYPE=Debug -DCMAKE_CXX_FLAGS="-O3" -B "$BUILD_DIR" -G Ninja .
+cmake -DENABLE_CLANG_TIDY=OFF -DCMAKE_BUILD_TYPE="$TYPE" -B "$BUILD_DIR" -G Ninja .
 cmake --build "$BUILD_DIR"
 mkdir -p /bos
 cp -r constants /bos
