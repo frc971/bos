0f034d712e83dc115b8e89bfeba68a495f2dad29
carrotmercinary@gmail.com <167659798+yasen5@users.noreply.github.com>
2026-04-01 04:46:53 +0000

    Remove changes to building

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,43 +3,26 @@ set -e
 
 # Defaults
 NAME=""
-TYPE="Release"
 
 # Parse args
 for arg in "$@"; do
   case $arg in
-    --name=*)
-      NAME="${arg#*=}"
-      shift
-      ;;
-    --type=*)
-      TYPE="${arg#*=}"
-      shift
-      ;;
+  --name=*)
+    NAME="${arg#*=}"
+    shift
+    ;;
   esac
 done
 
-# Normalize / validate type
-if [[ "$TYPE" != "Release" && "$TYPE" != "Debug" ]]; then
-  echo "Invalid --type: $TYPE (must be Release or Debug)"
-  exit 1
-fi
-
-# Lowercase version for directory naming
-TYPE_LOWER=$(echo "$TYPE" | tr '[:upper:]' '[:lower:]')
-
 # Determine build directory
 if [ -z "$NAME" ]; then
-  BUILD_DIR="${TYPE_LOWER}-build"
+  BUILD_DIR="build"
 else
-  BUILD_DIR="${NAME}-${TYPE_LOWER}-build"
+  BUILD_DIR="${NAME}-build"
 fi
 
-git submodule init
-git submodule update
-cmake -DENABLE_CLANG_TIDY=OFF -DCMAKE_BUILD_TYPE="$TYPE" -B "$BUILD_DIR" -G Ninja .
+git submodule update --init --progress --depth 1
+cmake -DENABLE_CLANG_TIDY=OFF -DCMAKE_BUILD_TYPE=Release -B "$BUILD_DIR" -G Ninja .
 cmake --build "$BUILD_DIR"
 mkdir -p /bos
-if [ "$(realpath constants)" != "$(realpath /bos/constants 2>/dev/null)" ]; then
-  cp -r constants /bos
-fi
+cp -r constants /bos
