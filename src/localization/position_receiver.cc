fb591f8d0fda5071694ec554bb53624b239066ac
Nano <nanoticity@gmail.com>
2026-04-18 06:14:08 +0000

    do changes, update position_reciever to use the /Pose/robotPose topic instead of just the left camera, the pose was really jumpy
    Signed-off-by: Nano <nanoticity@gmail.com>

--- src/localization/position_receiver.cc
+++ src/localization/position_receiver.cc
@@ -1,11 +1,13 @@
 #include "src/localization/position_receiver.h"
+#include <frc/geometry/Pose3d.h>
 
 namespace localization {
 
 PositionReceiver::PositionReceiver() {
   auto instance = nt::NetworkTableInstance::GetDefault();
+  auto pose_table = instance.GetTable("/Pose");
   nt::StructTopic<frc::Pose3d> pose3d_topic =
-      instance.GetStructTopic<frc::Pose3d>("/Orin/PoseEstimate/Left/Pose3d");
+      pose_table->GetStructTopic<frc::Pose3d>("robotPose");
   pose3d_subscriber_ = pose3d_topic.Subscribe({});
 }
 
