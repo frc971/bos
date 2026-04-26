3c0b7315db4888d4372306ec94085949d1551012
carrotmercinary@gmail.com <167659798+yasen5@users.noreply.github.com>
2026-04-26 02:12:36 +0000

    Joint solve (rebased)

--- src/utils/log.h
+++ src/utils/log.h
@@ -3,6 +3,7 @@
 #include <opencv2/core/mat.hpp>
 #include "absl/log/check.h"
 #include "absl/log/log.h"
+#include "src/utils/transform.h"
 
 namespace utils {
 void PrintPose3d(const frc::Pose3d& pose);
@@ -10,4 +11,9 @@ void PrintTransform3d(const frc::Transform3d& transform);
 void PrintTransformationMatrix(
     const cv::Mat& mat, const std::optional<std::string>& name = std::nullopt);
 void PrintPose2d(const frc::Pose2d& pose);
+template <typename Derived>
+inline void PrintTransformationMatrix(const Eigen::MatrixBase<Derived>& mat,
+                                      const std::optional<std::string>& name) {
+  PrintTransformationMatrix(utils::EigenToCvMat(mat));
+}
 }  // namespace utils
