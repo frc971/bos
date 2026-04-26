a162e069d831ab492256d6abd9367abb90365709
Charlie Huang <charliehuang09@gmail.com>
2026-04-26 02:26:45 +0000

    wip
    Signed-off-by: Charlie Huang <charliehuang09@gmail.com>

--- src/test/unit_test/forward_test.cc
+++ src/test/unit_test/forward_test.cc
@@ -230,32 +230,55 @@ const Eigen::Matrix<double, 4, 4> rotate_yaw =
 class ForwardTest : public ::testing::Test {
  protected:
   void SetUp() override {
-    camera_constant_ = GetCameraConstants().at("second_bot_right");
-
-    square_solver_ = std::make_unique<SquareSolver>(camera_constant_);
-    detector_ = std::make_unique<OpenCVAprilTagDetector>(
-        camera_constant_.frame_height.value(),
-        camera_constant_.frame_height.value(),
-        ReadIntrinsics(camera_constant_.intrinsics_path.value()));
-    camera_matrix_ = CameraMatrixFromJson<Eigen::Matrix3d>(
-        ReadIntrinsics(camera_constant_.intrinsics_path.value()));
-    normalized_camera_matrix_ = camera_matrix_ / NORMALIZATION;
-    normalized_camera_matrix_(0, 0) = 1;
-
-    camera_to_robot_ =
+    camera_constant_right_ = GetCameraConstants().at("second_bot_right");
+    square_solver_right_ =
+        std::make_unique<SquareSolver>(camera_constant_right_);
+    detector_right_ = std::make_unique<OpenCVAprilTagDetector>(
+        camera_constant_right_.frame_width.value(),
+        camera_constant_right_.frame_height.value(),
+        ReadIntrinsics(camera_constant_right_.intrinsics_path.value()));
+    camera_matrix_right_ = CameraMatrixFromJson<Eigen::Matrix3d>(
+        ReadIntrinsics(camera_constant_right_.intrinsics_path.value()));
+    normalized_camera_matrix_right_ = camera_matrix_right_ / NORMALIZATION;
+    normalized_camera_matrix_right_(0, 0) = 1;
+    camera_to_robot_right_ =
         utils::ExtrinsicsJsonToCameraToRobot(
-            utils::ReadExtrinsics(camera_constant_.extrinsics_path.value()))
+            utils::ReadExtrinsics(
+                camera_constant_right_.extrinsics_path.value()))
+            .ToMatrix();
+
+    camera_constant_left_ = GetCameraConstants().at("second_bot_left");
+    square_solver_left_ = std::make_unique<SquareSolver>(camera_constant_left_);
+    detector_left_ = std::make_unique<OpenCVAprilTagDetector>(
+        camera_constant_left_.frame_width.value(),
+        camera_constant_left_.frame_height.value(),
+        ReadIntrinsics(camera_constant_left_.intrinsics_path.value()));
+    camera_matrix_left_ = CameraMatrixFromJson<Eigen::Matrix3d>(
+        ReadIntrinsics(camera_constant_left_.intrinsics_path.value()));
+    normalized_camera_matrix_left_ = camera_matrix_left_ / NORMALIZATION;
+    normalized_camera_matrix_left_(0, 0) = 1;
+    camera_to_robot_left_ =
+        utils::ExtrinsicsJsonToCameraToRobot(
+            utils::ReadExtrinsics(
+                camera_constant_left_.extrinsics_path.value()))
             .ToMatrix();
   }
 
   void TearDown() override {}
 
-  camera_constant_t camera_constant_;
-  std::unique_ptr<SquareSolver> square_solver_;
-  std::unique_ptr<OpenCVAprilTagDetector> detector_;
-  Eigen::Matrix3d camera_matrix_;
-  Eigen::Matrix3d normalized_camera_matrix_;
-  Eigen::Matrix4d camera_to_robot_;
+  std::unique_ptr<OpenCVAprilTagDetector> detector_right_;
+  camera_constant_t camera_constant_right_;
+  std::unique_ptr<SquareSolver> square_solver_right_;
+  Eigen::Matrix3d camera_matrix_right_;
+  Eigen::Matrix3d normalized_camera_matrix_right_;
+  Eigen::Matrix4d camera_to_robot_right_;
+
+  std::unique_ptr<OpenCVAprilTagDetector> detector_left_;
+  camera_constant_t camera_constant_left_;
+  std::unique_ptr<SquareSolver> square_solver_left_;
+  Eigen::Matrix3d camera_matrix_left_;
+  Eigen::Matrix3d normalized_camera_matrix_left_;
+  Eigen::Matrix4d camera_to_robot_left_;
 };
 
 auto ProjectPoints(const frc::Pose3d& camera_pose, const frc::Pose3d& tag_pose,
@@ -338,7 +361,7 @@ void CheckIsEqual(cv::Point2d image_point, Eigen::Vector3d projected_points,
   EXPECT_NEAR(image_point.y, projected_points[2], tolerance);
 }
 
-TEST_F(ForwardTest, ProjectTest) {  // NOLINT
+TEST_F(ForwardTest, TestForward) {  // NOLINT
   // [u, v] = camera_matrix * PI * camera_to_tag * tag_corners
   //
   // feild_to_camera = feild_to_tag * 180_yaw * camera_to_tag.inv
@@ -361,25 +384,25 @@ TEST_F(ForwardTest, ProjectTest) {  // NOLINT
     cv::Mat image = cv::imread(image_paths[i]);
     timestamped_frame_t timestamped_frame{
         .frame = std::move(image), .timestamp = 0, .invalid = false};
-    auto detections = detector_->GetTagDetections(timestamped_frame);
+    auto detections = detector_right_->GetTagDetections(timestamped_frame);
     if (detections.empty()) {
       continue;
     }
     auto detection = detections[0];
 
     auto square_solver_solution =
-        square_solver_->EstimatePosition({detection})[0];
+        square_solver_right_->EstimatePosition({detection})[0];
 
     for (int j = 0; j < 4; j++) {
       auto projected_points =
           ProjectPoints(square_solver_solution.pose,
                         kapriltag_layout.GetTagPose(detection.tag_id).value(),
-                        camera_matrix_, camera_to_robot_, j);
+                        camera_matrix_right_, camera_to_robot_right_, j);
 
-      auto projected_points_normalized =
-          ProjectPoints(square_solver_solution.pose,
-                        kapriltag_layout.GetTagPose(detection.tag_id).value(),
-                        normalized_camera_matrix_, camera_to_robot_, j);
+      auto projected_points_normalized = ProjectPoints(
+          square_solver_solution.pose,
+          kapriltag_layout.GetTagPose(detection.tag_id).value(),
+          normalized_camera_matrix_right_, camera_to_robot_right_, j);
 
       CheckIsEqual(detection.corners[j], projected_points, 1);
       CheckIsEqual(detection.corners[j] / NORMALIZATION,
@@ -392,11 +415,11 @@ TEST_F(ForwardTest, TestDerrivative) {  // NOLINT
   cv::Mat image = cv::imread("/bos-logs/log181/right/7.047703.jpg");
   timestamped_frame_t timestamped_frame{
       .frame = std::move(image), .timestamp = 0, .invalid = false};
-  auto detections = detector_->GetTagDetections(timestamped_frame);
+  auto detections = detector_right_->GetTagDetections(timestamped_frame);
   auto detection = detections[0];
 
   auto square_solver_solution =
-      square_solver_->EstimatePosition({detection})[0];
+      square_solver_right_->EstimatePosition({detection})[0];
 
   auto feild_to_tag =
       kapriltag_layout.GetTagPose(detection.tag_id).value().ToMatrix();
@@ -421,15 +444,14 @@ TEST_F(ForwardTest, TestDerrivative) {  // NOLINT
                              .finished();
 
       robot_to_feild_d += CalculateDerivative(
-          robot_to_feild, feild_to_tag, camera_to_robot_,
-          normalized_camera_matrix_, image_point, corner_index);
+          robot_to_feild, feild_to_tag, camera_to_robot_right_,
+          normalized_camera_matrix_right_, image_point, corner_index);
 
-      loss +=
-          CalculateLoss(robot_to_feild, feild_to_tag, camera_to_robot_,
-                        normalized_camera_matrix_, image_point, corner_index);
+      loss += CalculateLoss(
+          robot_to_feild, feild_to_tag, camera_to_robot_right_,
+          normalized_camera_matrix_right_, image_point, corner_index);
     }
     robot_to_feild -= robot_to_feild_d * LR;
-    LOG(INFO) << loss;
   }
   ASSERT_LT(loss, 0.001);
 }
@@ -438,11 +460,11 @@ TEST_F(ForwardTest, TestTransfrom3dConstructor) {  // NOLINT
   cv::Mat image = cv::imread("/bos-logs/log181/right/7.047703.jpg");
   timestamped_frame_t timestamped_frame{
       .frame = std::move(image), .timestamp = 0, .invalid = false};
-  auto detections = detector_->GetTagDetections(timestamped_frame);
+  auto detections = detector_right_->GetTagDetections(timestamped_frame);
   auto detection = detections[0];
 
   auto square_solver_solution =
-      square_solver_->EstimatePosition({detection})[0];
+      square_solver_right_->EstimatePosition({detection})[0];
 
   DifferntiableTransform3d transform_from_pose(square_solver_solution.pose);
   DifferntiableTransform3d transform_from_eigen(
@@ -468,11 +490,11 @@ TEST_F(ForwardTest, TestTransfrom3d) {  // NOLINT
   cv::Mat image = cv::imread("/bos-logs/log181/right/7.047703.jpg");
   timestamped_frame_t timestamped_frame{
       .frame = std::move(image), .timestamp = 0, .invalid = false};
-  auto detections = detector_->GetTagDetections(timestamped_frame);
+  auto detections = detector_right_->GetTagDetections(timestamped_frame);
   auto detection = detections[0];
 
   auto square_solver_solution =
-      square_solver_->EstimatePosition({detection})[0];
+      square_solver_right_->EstimatePosition({detection})[0];
 
   DifferntiableTransform3d transform(square_solver_solution.pose);
   transform.CalculateMatrix();
@@ -488,16 +510,18 @@ TEST_F(ForwardTest, TestTransfrom3d) {  // NOLINT
   }
 }
 
-TEST_F(ForwardTest, TestBackpropagation) {  // NOLINT
+TEST_F(ForwardTest, TestMultiTagBackpropagation) {  // NOLINT
   cv::Mat image = cv::imread("/bos-logs/log181/right/20.411762.jpg");
   // LOG(INFO) << detections.size();
   timestamped_frame_t timestamped_frame{
       .frame = std::move(image), .timestamp = 0, .invalid = false};
-  auto detections = detector_->GetTagDetections(timestamped_frame);
-  auto detection = detections[0];
+  auto detections = detector_right_->GetTagDetections(timestamped_frame);
+
+  // PCHECK(detections.size() > 1)
+  //     << "Need at least two detections to test differnt tag positions";
 
   auto square_solver_solution =
-      square_solver_->EstimatePosition({detection})[0];
+      square_solver_right_->EstimatePosition(detections)[0];
 
   auto feild_to_robot = square_solver_solution.pose.ToMatrix();
 
@@ -519,35 +543,127 @@ TEST_F(ForwardTest, TestBackpropagation) {  // NOLINT
   for (int epoch = 0; epoch < EPOCHS; epoch++) {
     loss = 0;
     transform3d_derrivative_t derrivative;
+    for (const auto& detection : detections) {
+      auto feild_to_tag =
+          kapriltag_layout.GetTagPose(detection.tag_id).value().ToMatrix();
 
-    auto feild_to_tag =
-        kapriltag_layout.GetTagPose(detection.tag_id).value().ToMatrix();
+      for (int corner_index = 0; corner_index < 4; corner_index++) {
+        robot_to_feild.CalculateMatrix();
+        robot_to_feild.RegisterInputs(tape);
+        const auto robot_to_feild_eigen = robot_to_feild.ToEigen();
+        auto image_point = (Eigen::Vector3d() << 1,
+                            detection.corners[corner_index].x / NORMALIZATION,
+                            detection.corners[corner_index].y / NORMALIZATION)
+                               .finished();
 
-    for (int corner_index = 0; corner_index < 4; corner_index++) {
-      robot_to_feild.CalculateMatrix();
-      robot_to_feild.RegisterInputs(tape);
-      const auto robot_to_feild_eigen = robot_to_feild.ToEigen();
-      auto image_point = (Eigen::Vector3d() << 1,
-                          detection.corners[corner_index].x / NORMALIZATION,
-                          detection.corners[corner_index].y / NORMALIZATION)
-                             .finished();
+        auto robot_to_feild_d = CalculateDerivative(
+            robot_to_feild_eigen, feild_to_tag, camera_to_robot_right_,
+            normalized_camera_matrix_right_, image_point, corner_index);
 
-      auto robot_to_feild_d = CalculateDerivative(
-          robot_to_feild_eigen, feild_to_tag, camera_to_robot_,
-          normalized_camera_matrix_, image_point, corner_index);
+        robot_to_feild.RegisterOutputs(tape);
+        derrivative =
+            derrivative + robot_to_feild.BackPropagate(robot_to_feild_d, tape);
 
-      robot_to_feild.RegisterOutputs(tape);
-      derrivative =
-          derrivative + robot_to_feild.BackPropagate(robot_to_feild_d, tape);
+        loss += CalculateLoss(
+            robot_to_feild_eigen, feild_to_tag, camera_to_robot_right_,
+            normalized_camera_matrix_right_, image_point, corner_index);
 
-      loss +=
-          CalculateLoss(robot_to_feild_eigen, feild_to_tag, camera_to_robot_,
-                        normalized_camera_matrix_, image_point, corner_index);
-
-      tape.newRecording();
+        tape.newRecording();
+      }
     }
     robot_to_feild.Apply(derrivative);
   }
-  ASSERT_LT(solve_timer.Stop(), 1.0 / 250);
+  ASSERT_LT(solve_timer.Stop(), 0.1);
   ASSERT_LT(loss, 0.01);
+}
+TEST_F(ForwardTest, TestMultiCameraBackpropagation) {  // NOLINT
+  cv::Mat image_right = cv::imread("/bos-logs/log181/right/20.411762.jpg");
+
+  cv::Mat image_left = cv::imread("/bos-logs/log181/left/20.935361.jpg");
+  std::map<std::string, std::vector<tag_detection_t>> detections;
+  {
+    timestamped_frame_t timestamped_frame{
+        .frame = std::move(image_right), .timestamp = 0, .invalid = false};
+    detections[camera_constant_right_.name] =
+        detector_right_->GetTagDetections(timestamped_frame);
+    CHECK(!detections[camera_constant_right_.name].empty());
+  }
+  {
+    timestamped_frame_t timestamped_frame{
+        .frame = std::move(image_left), .timestamp = 0, .invalid = false};
+    detections[camera_constant_left_.name] =
+        detector_left_->GetTagDetections(timestamped_frame);
+    CHECK(!detections[camera_constant_left_.name].empty());
+  }
+
+  auto square_solver_solution = square_solver_right_->EstimatePosition(
+      detections[camera_constant_right_.name])[0];
+
+  auto feild_to_robot = square_solver_solution.pose.ToMatrix();
+  DifferntiableTransform3d robot_to_feild(feild_to_robot.inverse());
+
+  // Noise
+  robot_to_feild.t_x += 0.1;
+  robot_to_feild.t_y += 0.1;
+  robot_to_feild.t_z += 0.1;
+
+  robot_to_feild.r_x += 0.1;
+  robot_to_feild.r_y += 0.1;
+  robot_to_feild.r_z += 0.1;
+
+  tape_type tape;
+
+  double loss = 0;
+  std::vector<camera_constant_t> camera_constants{camera_constant_left_,
+                                                  camera_constant_right_};
+  utils::Timer solve_timer("solve", false);
+  for (int epoch = 0; epoch < EPOCHS; epoch++) {
+    loss = 0;
+    transform3d_derrivative_t derrivative;
+    for (const auto& camera_constant : camera_constants) {
+
+      auto camera_to_robot =
+          utils::ExtrinsicsJsonToCameraToRobot(
+              utils::ReadExtrinsics(camera_constant.extrinsics_path.value()))
+              .ToMatrix();
+
+      auto camera_matrix = CameraMatrixFromJson<Eigen::Matrix3d>(
+          ReadIntrinsics(camera_constant.intrinsics_path.value()));
+      Eigen::Matrix3d normalized_camera_matrix = camera_matrix / NORMALIZATION;
+      normalized_camera_matrix(0, 0) = 1;
+
+      for (const auto& detection : detections[camera_constant.name]) {
+        auto feild_to_tag =
+            kapriltag_layout.GetTagPose(detection.tag_id).value().ToMatrix();
+
+        for (int corner_index = 0; corner_index < 4; corner_index++) {
+          robot_to_feild.CalculateMatrix();
+          robot_to_feild.RegisterInputs(tape);
+          const auto robot_to_feild_eigen = robot_to_feild.ToEigen();
+          auto image_point = (Eigen::Vector3d() << 1,
+                              detection.corners[corner_index].x / NORMALIZATION,
+                              detection.corners[corner_index].y / NORMALIZATION)
+                                 .finished();
+
+          auto robot_to_feild_d = CalculateDerivative(
+              robot_to_feild_eigen, feild_to_tag, camera_to_robot,
+              normalized_camera_matrix, image_point, corner_index);
+
+          robot_to_feild.RegisterOutputs(tape);
+          derrivative = derrivative +
+                        robot_to_feild.BackPropagate(robot_to_feild_d, tape);
+
+          loss += CalculateLoss(robot_to_feild_eigen, feild_to_tag,
+                                camera_to_robot, normalized_camera_matrix,
+                                image_point, corner_index);
+
+          tape.newRecording();
+        }
+      }
+    }
+    robot_to_feild.Apply(derrivative);
+    // LOG(INFO) << loss;
+  }                                    // The test is very unoptimized
+  ASSERT_LT(solve_timer.Stop(), 0.1);  // The test is very unoptimized
+  ASSERT_LT(loss, 50);
 }
