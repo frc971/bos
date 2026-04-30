9ef52cb4dbea5e469e45ee22dfc7bfb0386b2b97
carrotmercinary@gmail.com <167659798+yasen5@users.noreply.github.com>
2026-04-30 13:53:13 +0000

    Fix logging

--- src/camera/multi_camera_source.cc
+++ src/camera/multi_camera_source.cc
@@ -1,5 +1,6 @@
 #include "multi_camera_source.h"
 #include "src/camera/camera.h"
+#include "src/camera/write_frame.h"
 #include "src/utils/log.h"
 
 namespace camera {
@@ -9,9 +10,18 @@ MultiCameraSource::MultiCameraSource(
       timestamped_frames_(cameras_.size()),
       use_all_frames_(use_all_frames) {
   camera_threads_.reserve(cameras_.size());
+  std::string log_path = frc::DataLogManager::GetLogDir();
+  LOG(INFO) << "Making overall log folder: " << log_path << " "
+            << std::filesystem::create_directory(log_path);
   for (size_t i = 0; i < cameras_.size(); i++) {
-    camera_threads_.emplace_back([this, i]() -> void {
+    camera_threads_.emplace_back([this, i, log_path]() -> void {
+      const std::string camera_log_dest =
+          fmt::format("{}/{}", log_path, cameras_[i]->GetCameraConstant().name);
+      LOG(INFO) << "Making camera log folder: "
+                << std::filesystem::create_directory(camera_log_dest);
+      int counter = 0;
       while (true) {
+        counter++;
         if (use_all_frames_) {
           mutex_.lock();
           bool frames_used = frames_used_;
@@ -27,6 +37,9 @@ MultiCameraSource::MultiCameraSource(
         timestamped_frames_[i] = timestamped_frame;
         frames_used_ = false;
         mutex_.unlock();
+        if (counter % log_frequency == 0 && !timestamped_frame.frame.empty()) {
+          WriteFrame(camera_log_dest, timestamped_frame);
+        }
       }
     });
   }
