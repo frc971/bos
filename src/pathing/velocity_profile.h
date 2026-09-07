b363f86b2477c2d53f933c000ccfd8cbf168978a
Ari <nanoticity@gmail.com>
2026-09-07 04:22:51 +0000

    Add less points at start and end so robot no jitter
    Signed-off-by: Ari <nanoticity@gmail.com>

--- src/pathing/velocity_profile.h
+++ src/pathing/velocity_profile.h
@@ -7,8 +7,8 @@
 namespace pathing {
 
 // Gotten from current pathplanner config of team
-const double max_velocity = 3.5;
-const double max_accel = 3.2;
+const double max_velocity = 2;
+const double max_accel = 2;
 
 auto _CreateVelocityProfile(const SplineResult& result, double maxVelocity,
                             double maxAcceleration,
